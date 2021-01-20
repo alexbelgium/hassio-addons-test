408ec45109f6616d1b329d9be89303f9fc595a76
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-01-20 23:07:21 +0100

    A

--- bitwarden/run.sh
+++ bitwarden/run.sh
@@ -9,11 +9,10 @@ declare request_size_limit
 declare secret_key
 
 # Set defaults
+export ROCKET_TLS="{certs="$(bashio::config 'certfile')",key="$(bashio::config 'keyfile')"}" \
 export DATA_FOLDER=/data
 export ROCKET_PORT=7277
 export ROCKET_WORKERS=2
-
-export ROCKET_TLS="{certs="/$(bashio::config 'certfile')",key="/$(bashio::config 'keyfile')"}" \
 
 # Set a random secret, to remove confusing warning from logs.
 secret_key=$(openssl rand -base64 32)
