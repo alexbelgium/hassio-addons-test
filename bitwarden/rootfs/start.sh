85b21ef985f3460fd99e4ad75f78785746b8d0ca
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-01-21 12:03:37 +0100

    1

--- bitwarden/rootfs/start.sh
+++ bitwarden/rootfs/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+chmod 777 /etc/cont-init.d/nginx.sh
 # Start Nginx
 ./etc/cont-init.d/nginx.sh -D
 status=$?
@@ -9,17 +10,21 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start bitwarden
+chmod 777 /etc/services.d/bitwarden/finish
 ./etc/services.d/bitwarden/finish -D
 status=$?
 
 # Start nginx
+chmod 777 /etc/services.d/nginx/finish
 ./etc/services.d/nginx/finish -D
 status=$?
 
 # Start bitwarden
+chmod 777 /etc/services.d/bitwarden/run
 ./etc/services.d/bitwarden/run -D
 status=$?
 
 # Start nginx
+chmod 777 /etc/services.d/nginx/run
 ./etc/services.d/nginx/run -D
 status=$?
