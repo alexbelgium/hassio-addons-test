a6f5cdbf15ce6ffbc1bbafe825768f3e6e0d780d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-01 14:46:18 +0100

    Update run.sh

--- paperlessng/run.sh
+++ paperlessng/run.sh
@@ -2,17 +2,17 @@
 
 #Populate variables https://paperless-ng.readthedocs.io/en/latest/configuration.html
 bashio::log.info "Setting variables according to configuration"
-export PAPERLESS_SECRET_KEY=$(bashio::config 'secretkey')
+# export PAPERLESS_SECRET_KEY=$(bashio::config 'secretkey')
 export PAPERLESS_OCR_LANGUAGE=$(bashio::config 'ocr_lang')
 export PAPERLESS_OCR_MODE=$(bashio::config 'ocr_mode')
 export DEFAULT_USERNAME=$(bashio::config 'username')
 export DEFAULT_EMAIL=$(bashio::config 'email')
 export DEFAULT_PASSWORD=$(bashio::config 'password')
-export PAPERLESS_CONSUMPTION_DIR=$(bashio::config 'consumption_dir')
-export PAPERLESS_DATA_DIR=$(bashio::config 'data_dir')
-export PAPERLESS_MEDIA_ROOT=$(bashio::config 'media_root')
-#export PAPERLESS_FILENAME_FORMAT==$(bashio::config 'format')
-#export PAPERLESS_ALLOWED_HOSTS==$(bashio::config 'whitelist')
+#export PAPERLESS_CONSUMPTION_DIR=$(bashio::config 'consumption_dir')
+#export PAPERLESS_DATA_DIR=$(bashio::config 'data_dir')
+#export PAPERLESS_MEDIA_ROOT=$(bashio::config 'media_root')
+export PAPERLESS_FILENAME_FORMAT==$(bashio::config 'format')
+export PAPERLESS_ALLOWED_HOSTS==$(bashio::config 'whitelist')
 
 #Optional variables
 if bashio::config.has_value 'UID'; then
