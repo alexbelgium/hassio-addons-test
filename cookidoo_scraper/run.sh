c29ebbc6be65d2b29d3803f4259a40a3715ed7ed
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-01 18:01:21 +0100

    test cookidoo

---
+++ cookidoo_scraper/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export =$('https://cookidoo.fr/recipes/recipe/fr-FR/')
+export COOKIDOO_HOME_URL=$('https://cookidoo.fr/foundation/fr-FR')
+export COOKIDOO_TIMELINE_URL=$('https://cookidoo.fr/planning/fr-FR/timeline/')
