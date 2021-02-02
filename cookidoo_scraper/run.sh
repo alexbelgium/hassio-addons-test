5e8cf1de04c30586bf35a4464c2895f2dfcead61
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-02 11:58:42 +0100

    removed

--- cookidoo_scraper/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export =$('https://cookidoo.fr/recipes/recipe/fr-FR/')
-export COOKIDOO_HOME_URL=$('https://cookidoo.fr/foundation/fr-FR')
-export COOKIDOO_TIMELINE_URL=$('https://cookidoo.fr/planning/fr-FR/timeline/')
