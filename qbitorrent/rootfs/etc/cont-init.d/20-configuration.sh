26795f2ad76cdcd87ceb0ba3bc11fcf42d3b56ba
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 16:46:07 +0100

    Update 20-configuration.sh

--- qbitorrent/rootfs/etc/cont-init.d/20-configuration.sh
+++ qbitorrent/rootfs/etc/cont-init.d/20-configuration.sh
@@ -2,6 +2,6 @@
 
 bashio::log.info "Setting variables"
 # MOFIFY DATA PATH
-sed -i "s|'/downloads/'|"$(bashio::config 'downloads')"|g" /defaults/qBittorrent.conf
-sed -i "s|'/downloads/incomplete/'|$(bashio::config 'temppath')|g" /defaults/qBittorrent.conf
+#sed -i "s|'/downloads/'|"$(bashio::config 'downloads')"|g" /defaults/qBittorrent.conf
+#sed -i "s|'/downloads/incomplete/'|$(bashio::config 'temppath')|g" /defaults/qBittorrent.conf
 echo 'WebUI\HostHeaderValidation=false' >> /defaults/qBittorrent.conf
