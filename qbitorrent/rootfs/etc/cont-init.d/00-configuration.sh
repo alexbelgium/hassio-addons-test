959371b11940964f39078629583912714da29e61
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 16:54:28 +0100

    Update 00-configuration.sh

--- qbitorrent/rootfs/etc/cont-init.d/00-configuration.sh
+++ qbitorrent/rootfs/etc/cont-init.d/00-configuration.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 bashio::log.info "Setting variables"
-# MOFIFY DATA PATH
-sed -i "s|'Downloads\SavePath=/downloads/'|"Downloads\SavePath=$(bashio::config 'downloads')"|g" /defaults/qBittorrent.conf
-sed -i "s|'Downloads\TempPath=/downloads/incomplete/'|Downloads\TempPath=$(bashio::config 'temppath')|g" /defaults/qBittorrent.conf
+sed '11,13d' /defaults/qBittorrent.conf
 echo 'WebUI\HostHeaderValidation=false' >> /defaults/qBittorrent.conf
 
 bashio::log.info "Default username/password : admin/adminadmin"
