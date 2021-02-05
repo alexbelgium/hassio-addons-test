fc3a7f67be61f6802bd32f6e24d74e4f1dd8f34c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 17:14:02 +0100

    Update and rename 00-configuration.sh to 50-configuration.sh

--- qbitorrent/rootfs/etc/cont-init.d/00-configuration.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-#bashio::log.info "Setting variables"
-#sed '11,13d' /defaults/qBittorrent.conf
-#echo 'WebUI\HostHeaderValidation=false' >> /defaults/qBittorrent.conf
-
-bashio::log.info "Default username/password : admin/adminadmin"
