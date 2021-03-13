2224ca06e607d268a9c0fd5bdc422a3eb4620b27
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-03-13 11:56:27 +0100

    Update nginx.sh

--- scrutiny/rootfs/etc/cont-init.d/nginx.sh
+++ scrutiny/rootfs/etc/cont-init.d/nginx.sh
@@ -1,27 +1,47 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Home Assistant Community Add-on: Node-RED
-# Configures NGINX for use with Node-RED
+# Home Assistant Community Add-on: AdGuard Home
+# Configures NGINX for use with the AdGuard Home server
 # ==============================================================================
-declare admin_port
+declare adguard_port=8080
+declare adguard_protocol=http
+declare tls_port
+
+# Figure out port settings from AdGuard
+if bashio::var.true "$(yq read /data/adguard/AdGuardHome.yaml tls.enabled)";
+then
+    tls_port=$(yq read /data/adguard/AdGuardHome.yaml tls.port_https)
+    if bashio::var.has_value "${tls_port}" && [[ "${tls_port}" -ne 0 ]]; then
+        adguard_port="${tls_port}"
+        adguard_protocol=https
+    fi
+fi
+
+# Generate upstream configuration
+bashio::var.json \
+    port "^${adguard_port}" \
+    | tempio \
+        -template /etc/nginx/templates/upstream.gtpl \
+        -out /etc/nginx/includes/upstream.conf
 
 # Generate Ingress configuration
 bashio::var.json \
     interface "$(bashio::addon.ip_address)" \
     port "^$(bashio::addon.ingress_port)" \
+    protocol "${adguard_protocol}" \
     | tempio \
         -template /etc/nginx/templates/ingress.gtpl \
         -out /etc/nginx/servers/ingress.conf
 
 # Generate direct access configuration, if enabled.
-admin_port=$(bashio::addon.port 80)
-if bashio::var.has_value "${admin_port}"; then
+if bashio::var.has_value "$(bashio::addon.port 80)"; then
     bashio::config.require.ssl
     bashio::var.json \
         certfile "$(bashio::config 'certfile')" \
         keyfile "$(bashio::config 'keyfile')" \
         leave_front_door_open "^$(bashio::config 'leave_front_door_open')" \
-        port "^${admin_port}" \
+        port "^$(bashio::addon.port 80)" \
+        protocol "${adguard_protocol}" \
         ssl "^$(bashio::config 'ssl')" \
         | tempio \
             -template /etc/nginx/templates/direct.gtpl \
