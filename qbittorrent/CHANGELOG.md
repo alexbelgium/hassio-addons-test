339f483393313e1a4f006cad5513bbe06a0db74c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-27 17:44:51 +0200

    qbittorrent

---
+++ qbittorrent/CHANGELOG.md
@@ -0,0 +1,59 @@
+
+## 14.3.4.99202104250604-7352-b2a43eeffubuntu20.04.1-ls129 (27-04-2021)
+- Update to latest version from linuxserver/docker-qbittorrent
+## 14.3.4.99202104250604-7352-b2a43eeffubuntu20.04.1-ls128
+- Update to latest version from linuxserver/docker-qbittorrent
+- Clarified steps to check in case of smb mount fail
+- New option : set domain for smb share
+- Addition of openvpn support 
+ 
+## 14.3.4.99202104180633-7350-2c8f322afubuntu20.04.1-ls127
+- Update to latest version from linuxserver/docker-qbittorrent
+
+## 14.3.4.99202104090418-7349-789803700ubuntu20.04.1-ls126
+- Update to latest version from linuxserver/docker-qbittorrent
+- Addition of ingress
+- Viewtorrent as default ui
+
+## 14.3.4.99202104031018-7348-2b6baa609ubuntu20.04.1-ls125
+- Update to latest version from linuxserver/docker-qbittorrent
+
+## 14.3.3.99202103251603-7345-332b173e0ubuntu20.04.1-ls122
+- Update to latest version from linuxserver/docker-qbittorrent ; Rebase to focal.
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu20.04.1-ls120
+- Update to latest version from linuxserver/docker-qbittorrent ; Stop creating /config/data directory on startup
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu20.04.1-ls119
+- Update to latest version from linuxserver/docker-qbittorrent ; Fix adding search engine plugin
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu20.04.1-ls118
+- Update to latest version from linuxserver/docker-qbittorrent
+- Allow mounting shares named \ip\share in addition to //ip/share
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu20.04.1-ls117
+- Update to latest version from linuxserver/docker-qbittorrent
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu20.04.1-ls116
+- Correct VueTorrent link
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu20.04.1-ls115
+- New configuration option : set download path
+- New configuration option : set username for webUI
+- New feature : mount smb share in protected mode
+- New feature : mount multiple smb shares
+- New config/feature : mount smbv1
+- Changed path : changed smb mount path from /storage/externalcifs to /mnt/$NAS name
+- Removed feature : ability to remove protection and mount local hdd, to increase the addon score
+- Update to latest version from linuxserver/docker-qbittorrent
+- Enabling of custom webUI from configuration
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu18.04.1-ls114
+- Update to latest version from linuxserver/docker-qbittorrent
+
+## 14.3.3.99202101191832-7248-da0b276d5ubuntu18.04.1-ls113
+- Update to latest version from linuxserver/docker-qbittorrent
+- Whitelist option
+- Enables PUID/GUID options
+- Addition of ssl options
+- Addition of possibility to mount local disc or smb
---
+++ qbittorrent/rootfs/etc/cont-init.d/91-configuration.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/with-contenv bashio
+
+##########
+# INIT   #
+##########
+
+# Define preferences line
+cd /config/qBittorrent/
+LINE=$(sed -n '/Preferences/=' qBittorrent.conf)
+LINE=$((LINE + 1))
+
+##################
+# Default folder #
+##################
+
+if bashio::config.has_value 'SavePath'; then
+  DOWNLOADS=$(bashio::config 'SavePath')
+  sed -i '/SavePath/d' qBittorrent.conf
+  sed -i "$LINE i\Downloads\\\SavePath=$DOWNLOADS" qBittorrent.conf
+  mkdir -p $DOWNLOADS || true
+  chown -R abc:abc $DOWNLOADS || bashio::log.info "Error, please check default save folder configuration in addon"
+  bashio::log.info "Downloads can be found in $DOWNLOADS"
+
+else
+  mkdir -p /share/qBittorrent || true
+  chown -R abc:abc /share/qBittorrent
+
+fi
+
+################
+# SSL CONFIG   #
+################
+
+# Clean data
+sed -i '/HTTPS/d' qBittorrent.conf
+
+bashio::config.require.ssl
+if bashio::config.true 'ssl'; then
+  bashio::log.info "ssl enabled. If webui don't work, disable ssl or check your certificate paths"
+  #set variables
+  CERTFILE=$(bashio::config 'certfile')
+  KEYFILE=$(bashio::config 'keyfile')
+  sed -i "$LINE i\WebUI\\\HTTPS\\\Enabled=True" qBittorrent.conf
+  sed -i "$LINE i\WebUI\\\HTTPS\\\CertificatePath=/ssl/$CERTFILE" qBittorrent.conf
+  sed -i "$LINE i\WebUI\\\HTTPS\\\KeyPath=/ssl/$KEYFILE" qBittorrent.conf
+fi
+
+################
+# WHITELIST    #
+################
+
+cd /config/qBittorrent/
+if bashio::config.has_value 'whitelist'; then
+  WHITELIST=$(bashio::config 'whitelist')
+  #clean data
+  sed -i '/AuthSubnetWhitelist/d' qBittorrent.conf
+  sed -i "$LINE i\WebUI\\\AuthSubnetWhitelistEnabled=true" qBittorrent.conf
+  sed -i "$LINE i\WebUI\\\AuthSubnetWhitelist=$WHITELIST" qBittorrent.conf
+  bashio::log.info "Whitelisted subsets will not require a password : $WHITELIST"
+fi
+
+###############
+# USERNAME    #
+###############
+
+cd /config/qBittorrent/
+if bashio::config.has_value 'Username'; then
+  USERNAME=$(bashio::config 'Username')
+  #clean data
+  sed -i '/WebUI\\\Username/d' qBittorrent.conf
+  #add data
+  sed -i "$LINE i\WebUI\\\Username=$USERNAME" qBittorrent.conf
+  bashio::log.info "WEBUI username set to $USERNAME"
+fi
+
+################
+# Alternate UI #
+################
+
+# Clean data
+sed -i '/AlternativeUIEnabled/d' qBittorrent.conf
+sed -i '/RootFolder/d' qBittorrent.conf
+rm -f -r /webui
+mkdir -p /webui
+
+if bashio::config.has_value 'customUI'; then
+  ### Variables
+  CUSTOMUI=$(bashio::config 'customUI')
+  bashio::log.info "Alternate UI enabled : $CUSTOMUI. If webui don't work, disable this option"
+
+  ### Download WebUI
+  case $CUSTOMUI in
+  "vuetorrent") 
+    curl -s -S -J -L -o /webui/release.zip $(curl -s https://api.github.com/repos/WDaan/VueTorrent/releases/latest | grep -o "http.*vuetorrent.zip") >/dev/null
+    ;;
+
+  "qbit-matUI")
+    curl -s -S -J -L -o /webui/release.zip $(curl -s https://api.github.com/repos/bill-ahmed/qbit-matUI/releases/latest | grep -o "http.*Unix.*.zip") >/dev/null
+    ;;
+
+  "qb-web")
+    curl -s -S -J -L -o /webui/release.zip $(curl -s https://api.github.com/repos/CzBiX/qb-web/releases | grep -o "http.*qb-web-.*zip") >/dev/null
+    ;;
+  esac
+
+  ### Install WebUI
+  mkdir -p /webui/$CUSTOMUI
+  unzip -q /webui/release.zip -d /webui/$CUSTOMUI
+  rm /webui/*.zip
+  CUSTOMUIDIR="$(dirname "$(find /webui/$CUSTOMUI -iname "public" -type d)")"
+  sed -i "$LINE i\WebUI\\\AlternativeUIEnabled=true" /config/qBittorrent/qBittorrent.conf
+  sed -i "$LINE i\WebUI\\\RootFolder=$CUSTOMUIDIR" /config/qBittorrent/qBittorrent.conf
+fi
+
+##########
+# CLOSE  #
+##########
+
+bashio::log.info "Default username/password : admin/adminadmin"
+bashio::log.info "Configuration can be found in /config/qBittorrent"
