c576f275853548319a737730043d4e9893959231
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-15 21:50:38 +0100

    Update 99-run.sh

--- fireflyiii/rootfs/etc/cont-init.d/99-run.sh
+++ fireflyiii/rootfs/etc/cont-init.d/99-run.sh
@@ -46,7 +46,7 @@ sqlite_internal)
 #    chown -R www-data:www-data /var/www/html/storage/database
 #    chmod 775 /var/www/html/storage/database
 #    touch /var/www/html/storage/database/database.sqlite
-    DB_DATABASE=/config/addons_config/fireflyiii/database/database.sqlite
+    export DB_DATABASE=/config/addons_config/fireflyiii/database/database.sqlite
     touch /config/addons_config/fireflyiii/database/database.sqlite
     ;;
 
