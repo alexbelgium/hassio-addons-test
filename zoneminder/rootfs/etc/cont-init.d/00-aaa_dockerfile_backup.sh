8c401cef9251fb6796e3c9dc4bc79c3b70018512
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-10 13:43:15 +0100

    update

--- zoneminder/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ zoneminder/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -6,14 +6,14 @@
 # Automatic modules download #
 ##############################
 if [ -e "/MODULESFILE" ]; then
-    echo "Executing modules script"
     MODULES=$(</MODULESFILE)
     MODULES="${MODULES:-00-banner.sh}"
-    
+    echo "Executing modules script : $MODULES"
+
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi \
     && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
     && mkdir -p /etc/cont-init.d \
-    && for scripts in $MODULES; do curl -L -f -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts" || (echo "script failed to install $scripts" && exit 1); done \
+    && for scripts in $MODULES; do echo "$scripts" && curl -f -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts" && [ "$(sed -n '/\/bin/p;q' /etc/cont-init.d/"$scripts")" != "" ] || (echo "script failed to install $scripts" && exit 1); done \
     && chmod -R 755 /etc/cont-init.d
 fi
 
@@ -21,12 +21,12 @@ fi
 # Automatic installer #
 #######################
 if [ -e "/ENVFILE" ]; then
-    echo "Executing script"
     PACKAGES=$(</ENVFILE)
-    
+    echo "Executing dependency script with custom elements : $PACKAGES"
+
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi \
     && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
-    && curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh \
+    && curl -f -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh \
     && chmod 777 /automatic_packages.sh \
     && eval /./automatic_packages.sh "${PACKAGES:-}" \
     && rm /automatic_packages.sh
