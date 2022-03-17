e2aca11e602920c2a739b31a486dbf1be1a748ab
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-17 20:25:59 +0100

    Update 32-nginx.sh

--- jellyfin/rootfs/etc/cont-init.d/32-nginx.sh
+++ jellyfin/rootfs/etc/cont-init.d/32-nginx.sh
@@ -31,3 +31,5 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
+# JELLYFIN_PublishedServerUrl
