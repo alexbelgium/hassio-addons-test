e695648f21cd3ca4418f110e1554313a37ff4bbe
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-19 21:22:43 +0100

    Update 32-nginx.sh

--- emby/rootfs/etc/cont-init.d/32-nginx.sh
+++ emby/rootfs/etc/cont-init.d/32-nginx.sh
@@ -9,7 +9,13 @@ declare ingress_interface
 declare ingress_port
 declare keyfile
 
+echo "Adapting for ingress"
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
+# Allow uppercase url
+echo "Allowing case sensitive url"
+apidirectory=$(dirname $(find / -name "connectionmanager.js"))
+grep -rl toLowerCase "$apidirectory" | xargs sed -i 's/toLowerCase()/toString()/g'
