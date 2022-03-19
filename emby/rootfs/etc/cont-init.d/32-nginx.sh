7f20fbab60347f1cdc267a56f06d1130c1935130
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-19 07:17:17 +0100

    Update 32-nginx.sh

--- emby/rootfs/etc/cont-init.d/32-nginx.sh
+++ emby/rootfs/etc/cont-init.d/32-nginx.sh
@@ -37,6 +37,3 @@ sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
 
 config_port=$(bashio::config 'port')
 config_path=$(bashio::config 'path')
-escaped_config_path=$(printf '%s\n' "$config_path" | sed -e 's/[\/&]/\\&/g')
-sed -i "s/%%config_port%%/${config_port}/g" /etc/nginx/includes/upstream.conf
-sed -i "s/%%config_path%%/${escaped_config_path}/g" /etc/nginx/servers/ingress.conf
