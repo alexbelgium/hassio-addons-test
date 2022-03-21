5d05efa45cbd9ab6644fb66338ec586c5da8aabe
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-21 09:14:33 +0100

    Update 32-nginx.sh

--- emby/rootfs/etc/cont-init.d/32-nginx.sh
+++ emby/rootfs/etc/cont-init.d/32-nginx.sh
@@ -14,3 +14,8 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
+# Allow uppercase url
+echo "Allowing case sensitive url"
+#grep -rl toLowerCase /app/emby/dashboard-ui/modules/emby-apiclient | xargs sed -i 's/toLowerCase()/toString()/g'
+grep -rl toLowerCase /app/emby/dashboard-ui | xargs sed -i 's/toLowerCase()/toString()/g'
