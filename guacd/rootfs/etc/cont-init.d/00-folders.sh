10aa8951c2059459487a927fc2856e3d43ee53b1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-04-01 11:03:17 +0200

    Update 00-folders.sh

--- guacd/rootfs/etc/cont-init.d/00-folders.sh
+++ guacd/rootfs/etc/cont-init.d/00-folders.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
+cp -rn /config/* /data
+
 # Define home
-HOME="/config/addons_config/guacd"
-mkdir -p "$HOME"/postgres
+#HOME="/config/addons_config/guacd"
+#mkdir -p "$HOME"/postgres
 
-PUID="$(bashio::config "PUID")"
-PGID="$(bashio::config "PGID")"
+#PUID="$(bashio::config "PUID")"
+#PGID="$(bashio::config "PGID")"
 
-mkdir -p "$HOME"
-chown -R "$PUID:$PGID" "$HOME"
+#mkdir -p "$HOME"
+#chown -R "$PUID:$PGID" "$HOME"
