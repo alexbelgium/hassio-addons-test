153b6ae1d5291f0ab9f043300815319a231ba03a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-13 17:56:44 +0200

    Update 10-script.sh

--- whoogle/rootfs/etc/cont-init.d/10-script.sh
+++ whoogle/rootfs/etc/cont-init.d/10-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s|/(self.value)|aze(self.value)|g" /whoogle/app/models/endpoint.py 
-sed -i "s|/{self.value}|aze{self.value}|g" /whoogle/app/models/endpoint.py
+sed -i "s|/(self.value)|.(self.value)|g" /whoogle/app/models/endpoint.py 
+#sed -i "s|/{self.value}|aze{self.value}|g" /whoogle/app/models/endpoint.py
