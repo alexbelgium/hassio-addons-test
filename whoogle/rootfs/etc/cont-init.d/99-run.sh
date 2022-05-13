925eb5e2940cee40ef9be6c254da3210cb3ac0d6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-13 09:16:03 +0200

    Update 99-run.sh

--- whoogle/rootfs/etc/cont-init.d/99-run.sh
+++ whoogle/rootfs/etc/cont-init.d/99-run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-exec misc/tor/start-tor.sh & ./run & bashio::net.wait_for 5000 localhost 900
-
-bashio::log.info "Starting NGinx..."
+exec misc/tor/start-tor.sh & ./run & echo "Starting NGinx..."
 
 exec nginx
