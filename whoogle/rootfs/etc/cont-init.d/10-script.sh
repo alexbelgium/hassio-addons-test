7149a2858fa1f1079594277dfc6dfe698e2d1ef0
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-13 22:26:51 +0200

    Update 10-script.sh

--- whoogle/rootfs/etc/cont-init.d/10-script.sh
+++ whoogle/rootfs/etc/cont-init.d/10-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod +x /share/azeaze.sh
-/./share/azeaze.sh
+chmod +x /config/azeaze.sh
+/./config/azeaze.sh
 
 #sed -i "s|path.startswith(self.value)|path.startswith(f'/{self.value}')|g" /whoogle/app/models/endpoint.py 
 #sed -i "s|/{self.value}|aze{self.value}|g" /whoogle/app/models/endpoint.py
