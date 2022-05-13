8269b866b050609806cef4ccf9293c48506455a4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-13 16:32:25 +0200

    Update 99-run.sh

--- whoogle/rootfs/etc/cont-init.d/99-run.sh
+++ whoogle/rootfs/etc/cont-init.d/99-run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sed -i "s|(self.value)|'aze'(self.value)|g" /whoogle/app/models/endpoint.py
+sed -i "s|(self.value)|'aze'{self.value}|g" /whoogle/app/models/endpoint.py
+
 exec misc/tor/start-tor.sh & ./run & echo "Starting NGinx..."
 
 exec nginx
