9de97754763a18b58a804d3a21d989f9fab9b76a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-14 17:07:02 +0200

    Delete 10-script.sh

--- whoogle/rootfs/etc/cont-init.d/10-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-chmod +x /config/azeaze.sh
-/./config/azeaze.sh
-
-#sed -i "s|path.startswith(self.value)|path.startswith(f'/{self.value}')|g" /whoogle/app/models/endpoint.py 
-#sed -i "s|/{self.value}|aze{self.value}|g" /whoogle/app/models/endpoint.py
