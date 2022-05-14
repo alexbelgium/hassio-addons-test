b1337c0dcaf9fa0b3ab8e8d458cb74c8a0ec2c5f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-14 17:10:14 +0200

    Update 99-run.sh

--- whoogle/rootfs/etc/cont-init.d/99-run.sh
+++ whoogle/rootfs/etc/cont-init.d/99-run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env
 
+#Allow images access
+#chmod -R 755 /whoogle/app
+#Allow manifest access 
+#sed -i 's|manifest.json">|manifest.json" crossorigin="use-credentials">|g' /whoogle/app/templates/index.html
+
+#Allow ingress
 WHOOGLE_URL_PREFIX="$(bashio::addon.ingress_entry)"
 export WHOOGLE_URL_PREFIX
 
