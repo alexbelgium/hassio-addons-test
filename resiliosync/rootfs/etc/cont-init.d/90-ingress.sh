70b429fd0e258deea673983f5a319c05d1b6cb0b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-20 14:10:37 +0200

    Update 90-ingress.sh

--- resiliosync/rootfs/etc/cont-init.d/90-ingress.sh
+++ resiliosync/rootfs/etc/cont-init.d/90-ingress.sh
@@ -11,5 +11,3 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-
-sed -i "s|/gui/|./gui/|g" /usr/bin/rslsync
