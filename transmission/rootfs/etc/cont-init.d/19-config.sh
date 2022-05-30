1daf4c33f4c4a50505c5289d2d61725c3618bb37
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-30 22:11:24 +0200

    Update 19-config.sh

--- transmission/rootfs/etc/cont-init.d/19-config.sh
+++ transmission/rootfs/etc/cont-init.d/19-config.sh
@@ -14,7 +14,7 @@ declare WHITELIST
 CONFIGDIR="$TRANSMISSION_HOME"
 
 for k in $(bashio::jq "${__BASHIO_ADDON_CONFIG}" 'keys | .[]'); do
-    sed -i "1a export $k=$(bashio::config $k)" /etc/openvpn/*
+    sed -i "3a export $k=$(bashio::config $k)" /etc/openvpn/*
 done
 
 ###############
