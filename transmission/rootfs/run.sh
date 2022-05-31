3392a7503b254fd27d70d406e7c8eb4cf1fd0b3f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-31 18:07:05 +0200

    Update run.sh

--- transmission/rootfs/run.sh
+++ transmission/rootfs/run.sh
@@ -6,5 +6,6 @@
 
 echo "Exporting variables"
 for k in $(bashio::jq "${__BASHIO_ADDON_CONFIG}" 'keys | .[]'); do
+    echo "export $k=$(bashio::config $k)"
     sed -i "1a export $k=$(bashio::config $k)" /etc/openvpn/start.sh
 done
