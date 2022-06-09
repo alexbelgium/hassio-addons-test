967b637335b600fc0eb2d9cd4b172e307cf4dddb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-06-09 11:13:50 +0200

    Create 99-run.sh

---
+++ seafile/rootfs/etc/cont-init.d/99-run.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bashio
+
+####################
+# GLOBAL VARIABLES #
+####################
+cp .env.example .env
+sed -i "s|NOSWAG=0|NOSWAG=1|g" .env
+sed -i "s|USE_HTTPS=1|USE_HTTPS=0|g" .env
+sed -i "s|PUID=1000|PUID=$(bashio::config 'PUID')|g" .env
+sed -i "s|PGID=1000|PGID=$(bashio::config 'PGID')|g" .env
+sed -i "s|TZ=Europe/Zurich|TZ=$(bashio::config 'TZ')|g" .env
+sed -i "s|URL=your.domain|URL=$(bashio::config 'SEAFILE_SERVER_HOSTNAME')|g" .env
+sed -i "s|SEAFILE_ADMIN_EMAIL=you@your.email|SEAFILE_ADMIN_EMAIL=$(bashio::config 'SEAFILE_ADMIN_EMAIL')|g" .env
+sed -i "s|SEAFILE_ADMIN_PASSWORD=secret|SEAFILE_ADMIN_PASSWORD=$(bashio::config 'SEAFILE_ADMIN_PASSWORD')|g" .env
+
+###################
+# Define database #
+###################
+bashio::log.info "Defining database"
+case $(bashio::config 'database') in
+
+    # Use sqlite
+    sed -i "s|SQLITE=0|SQLITE=1|g" .env
+    sqlite)
+    
+    # Use mariadb
+    mariadb_addon)
+        bashio::log.info "Using MariaDB addon. Requirements : running MariaDB addon. Discovering values..."
+        if ! bashio::services.available 'mysql'; then
+            bashio::log.fatal \
+                "Local database access should be provided by the MariaDB addon"
+            bashio::exit.nok \
+                "Please ensure it is installed and started"
+        fi
+
+        # Use values
+        sed -i "s|MYSQL_HOST=db|MYSQL_HOST=$(bashio::services "mysql" "host")|g" .env
+        sed -i "s|MYSQL_USER_PASSWD=secret|MYSQL_USER_PASSWD=$(bashio::services "mysql" "username")|g" .env
+        sed -i "s|MYSQL_ROOT_PASSWD=secret|MYSQL_USER_PASSWD=$(bashio::services "mysql" "password")|g" .env
+
+        bashio::log.warning "This addon is using the Maria DB addon"
+        bashio::log.warning "Please ensure this is included in your backups"
+        bashio::log.warning "Uninstalling the MariaDB addon will remove any data"
+        ;;
+esac
+
+##############
+# LAUNCH APP #
+##############
+
+bashio::log.info "Starting app"
+#/sbin/my_init -- /scripts/enterpoint.sh
+/./docker_entrypoint.sh
