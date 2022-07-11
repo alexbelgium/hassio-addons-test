d18b07a15501cfa01b752cf2df0f33e9af132066
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-07-11 18:20:12 +0200

    Update 32-ingress.sh

--- tandoor_recipes/rootfs/etc/cont-init.d/32-ingress.sh
+++ tandoor_recipes/rootfs/etc/cont-init.d/32-ingress.sh
@@ -13,3 +13,5 @@ ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
+
+sed -i 's|"/|"./|g' /opt/recipes/cookbook/static/django_js_reverse/*
