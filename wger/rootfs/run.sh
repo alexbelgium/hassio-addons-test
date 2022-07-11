fdd1998acb512d70eb673ef8cbadae22ca07d0f5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-07-11 14:39:01 +0200

    Update run.sh

--- wger/rootfs/run.sh
+++ wger/rootfs/run.sh
@@ -28,5 +28,13 @@ rmdir /home/wger/static && ln -s /home/wger/src/wger/core/static /home/wger || t
 su -l wger -c "\
   cd /home/wger/src && \
   export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
+  export DEBIAN_FRONTEND=noninteractive && \
+  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
+  export LANG=en_US.UTF-8 && \
+  export LANGUAGE=en_US:en && \
+  export LC_ALL=en_US.UTF-8 && \
+  export PATH=/home/wger/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin && \
+  export PYTHONDONTWRITEBYTECODE=1 && \
+  export PYTHONUNBUFFERED=1 && \
   /bin/bash /home/wger/entrypoint.sh \
   "
