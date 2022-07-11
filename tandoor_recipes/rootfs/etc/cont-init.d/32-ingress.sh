14fd76672d450e8dede85bbc83847f6f9cdd69c4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-07-11 18:41:23 +0200

    Update 32-ingress.sh

--- tandoor_recipes/rootfs/etc/cont-init.d/32-ingress.sh
+++ tandoor_recipes/rootfs/etc/cont-init.d/32-ingress.sh
@@ -14,4 +14,4 @@ sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
 
-sed -i 's|"/|"./|g' /opt/recipes/cookbook/static/django_js_reverse/*
+#sed -i 's|"/|"./|g' /opt/recipes/cookbook/static/django_js_reverse/*
