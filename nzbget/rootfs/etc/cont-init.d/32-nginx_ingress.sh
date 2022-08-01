5fb989033dbd670d0e93f0b5cbb13a8a2018fefb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-08-01 10:02:27 +0200

    Ingress test

---
+++ nzbget/rootfs/etc/cont-init.d/32-nginx_ingress.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+#################
+# NGINX SETTING #
+#################
+
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
