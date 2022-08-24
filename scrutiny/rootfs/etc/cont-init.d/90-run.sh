834e5e3303fa36abdaef6cc84a8510200ea45297
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-08-24 17:29:21 +0200

    Create 90-run.sh

---
+++ scrutiny/rootfs/etc/cont-init.d/90-run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+##################
+# URL CORRECTION #
+##################
+
+# allow true url for ingress
+grep -rl '/web/' /opt/scrutiny/web/ | xargs sed -i 's|/web/|./|g'
+grep -rl '/api/' /app | xargs sed -i 's|/api/|api/|g' || true
+grep -rl 'api/' /app | xargs sed -i 's|api/|./api/|g' || true
+
+################
+# CRON OPTIONS #
+################
+
+#rm /config/crontabs/* || true
+#sed -i '$d' /etc/crontabs/root
+#sed -i -e '$a @reboot /run.sh' /etc/crontabs/root
+
+# Align update with options
+#FREQUENCY=$(bashio::config 'Updates')
+#bashio::log.info "$FREQUENCY updates"
+
+#case $FREQUENCY in
+#    "Hourly")
+#        sed -i -e '$a 0 * * * * /run.sh' /etc/crontabs/root
+#        ;;
+#
+#    "Daily")
+#        sed -i -e '$a 0 0 * * * /run.sh' /etc/crontabs/root
+#        ;;
+#
+#    "Weekly")
+#        sed -i -e '$a 0 0 * * 0 /run.sh' /etc/crontabs/root
+#        ;;
+#esac
