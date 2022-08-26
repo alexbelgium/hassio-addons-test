ccebc1833e3d81cc30285a0ffd87ac172fb91670
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-08-26 17:56:06 +0200

    Update and rename 31-run.sh to 01-run.sh

--- scrutiny/rootfs/etc/cont-init.d/01-run.sh
+++ scrutiny/rootfs/etc/cont-init.d/01-run.sh
@@ -5,25 +5,37 @@
 # Create folder #
 #################
 
+echo "Creating folders structure"
 mkdir -p /config/addons_config/scrutiny/config
 mkdir -p /config/addons_config/scrutiny/influxdb
-
-cp /opt/scrutiny/config/* /config/addons_config/scrutiny/config/
-cp /opt/scrutiny/influxdb/* /config/addons_config/scrutiny/influxdb/
+cp -rn /opt/scrutiny/config/* /config/addons_config/scrutiny/config/
+cp -rn /opt/scrutiny/influxdb/* /config/addons_config/scrutiny/influxdb/
 rm -r /opt/scrutiny/config
 rm -r /opt/scrutiny/influxdb
 ln -s /config/addons_config/scrutiny/config /opt/scrutiny
 ln -s /config/addons_config/scrutiny/influxdb /opt/scrutiny
+
+######
+# TZ #
+######
+
+# Align timezone with options
+TZ="$(bashio::config 'TZ')"
+bashio::log.info "Timezone : $TZ"
+
+if bashio::config.has_value "TZ"; then
+sed -i "1a export TZ=$TZ" /etc/cont-init.d/10-timezone
+fi
 
 ################
 # CRON OPTIONS #
 ################
 
 # Align update with options
-FREQUENCY=$(bashio::config 'Updates')
+FREQUENCY="$(bashio::config 'Updates')"
 bashio::log.info "$FREQUENCY updates"
 
-case $FREQUENCY in
+case "$FREQUENCY" in
     "Hourly")
         sed -i "1a export COLLECTOR_CRON_SCHEDULE=\"0 * * * *\"" /etc/cont-init.d/50-cron-config
         ;;
