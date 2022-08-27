d0b6f333cf0f4785a35d11c01a41dcc816866493
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-08-27 19:46:06 +0200

    Update 99-run.sh

--- tandoor_recipes/rootfs/etc/cont-init.d/99-run.sh
+++ tandoor_recipes/rootfs/etc/cont-init.d/99-run.sh
@@ -13,13 +13,7 @@ export SECRET_KEY=$(bashio::config 'SECRET_KEY') && bashio::log.blue "SECRET_KEY
 # Allow ingress #
 #################
 
-if [ -f /config/tandoortest.sh ]; then echo "running test file" && /./config/tandoortest.sh; fi
-#bashio::log.info "Setting ingress"
-#ingress_entry="$(bashio::addon.ingress_entry)"
-#export SCRIPT_NAME="$ingress_entry"
-#export JS_REVERSE_SCRIPT_PREFIX="${ingress_entry}/"
-#export STATIC_URL="${ingress_entry}/static/"
-#export MEDIA_URL="${ingress_entry}/media/"
+sed -i "s|href=\"{% base_path request \'base\' %}\"|href=\"{% base_path request \'base\' %}/\"|g" /opt/recipes/cookbook/templates/base.html
 
 ###################
 # Define database #
