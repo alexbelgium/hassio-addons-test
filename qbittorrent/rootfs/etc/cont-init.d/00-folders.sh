96495a4f8e64196122a68e3849d1a26eaa1e4264
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-09-10 23:27:49 +0200

    Update 00-folders.sh

--- qbittorrent/rootfs/etc/cont-init.d/00-folders.sh
+++ qbittorrent/rootfs/etc/cont-init.d/00-folders.sh
@@ -3,6 +3,6 @@
 mkdir -p /config/addons_config/qBittorrent
 
 if [ -d /config/qBittorrent ]; then
-    cp /config/qBittorrent/* /config/addons_config/qBittorrent/*
+    mv /config/qBittorrent/* /config/addons_config/qBittorrent/
     echo "Files were moved to /config/addons_config/qBittorrent" > /config/qBittorrent/filesmoved
 fi
