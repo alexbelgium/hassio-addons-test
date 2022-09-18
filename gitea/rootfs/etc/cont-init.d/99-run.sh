f225e0443e46e7b0497817cb060b1b689195697d
Alexandre <alexandre.pary@gmail.com>
2022-09-18 17:48:31 +0200

    update

--- gitea/rootfs/etc/cont-init.d/99-run.sh
+++ gitea/rootfs/etc/cont-init.d/99-run.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bashio
 # shellcheck shell=bash
 
-#sed -i "1a export SITE_TITLE=$(bashio::config 'SITE_TITLE')" /usr/bin/entrypoint
-#sed -i "1a export SERVER_DOMAIN=$(bashio::config 'SERVER_DOMAIN')" /usr/bin/entrypoint
-#sed -i "1a export BASE_URL=$(bashio::config 'BASE_URL')" /usr/bin/entrypoint
+for file in /data/gitea/conf/app.ini /etc/templates/app.ini; do
 
-echo "SITE_TITLE=$(bashio::config 'SITE_TITLE')" >> /.env
-echo "SERVER_DOMAIN=$(bashio::config 'SERVER_DOMAIN')" >> /.env
-echo "BASE_URL=$(bashio::config 'BASE_URL')" >> /.env
+##############
+# OTHER CONF #
+##############
 
-#echo "site title $SITE_TITLE"
-#echo "server domain $SERVER_DOMAIN"
-#echo "base url $BASE_URL"
+for param in APP_NAME DOMAIN ROOT_URL; do
+  if bashio::config.has_value "$param"; then
+    echo "parameter set : $param=$(bashio::config '$param')"
+    sed -i "/$param/d" "$file"
+    sed -i "/server/a $param = $(bashio::config '$param')" "$file"
+  fi
+done
 
-#sed "s/^APP.*/APP      = $SITE_TITLE/" /data/gitea/conf/app.ini
-#sed "s/^DOMAIN.*/DOMAIN      = $SERVER_DOMAIN/" /data/gitea/conf/app.ini
-#sed "s/^ROOT_URL.*/ROOT_URL       = $BASE_URL/" /data/gitea/conf/app.ini
+##############
+# SSL CONFIG #
+##############
 
+# Clean values
+sed -i "/PROTOCOL/d" "$file"
+sed -i "/CERT_FILE/d" "$file"
+sed -i "/KEY_FILE/d" "$file"
+
+# Add ssl
 bashio::config.require.ssl
-#sed "/PROTOCOL/d " /data/gitea/conf/app.ini
-#sed "/CERT_FILE/d " /data/gitea/conf/app.ini
-#sed "/KEY_FILE/d " /data/gitea/conf/app.ini
 if bashio::config.true 'ssl'; then
-export CERT_FILE=/ssl/$(bashio::config 'certfile')
-export KEY_FILE=/ssl/$(bashio::config 'keyfile')
-export PROTOCOL=https
-	#sed "/server/a PROTOCOL  = https" /data/gitea/conf/app.ini
-	#sed "/server/a ROOT_URL  = https://$BASE_URL:3000/" /data/gitea/conf/app.ini
-	#sed "/server/a CERT_FILE = $(bashio::config 'certfile')" /data/gitea/conf/app.ini
-	#sed "/server/a KEY_FILE = $(bashio::config 'keyfile')" /data/gitea/conf/app.ini
-#else
-	#sed "/server/a PROTOCOL  = http" /data/gitea/conf/app.ini
-	#sed "/server/a ROOT_URL  = http://$BASE_URL:3000/" /data/gitea/conf/app.ini
+bashio::log.info "ssl is enabled"
+sed -i "/server/a PROTOCOL = https" "$file"
+sed -i "/server/a CERT_FILE = /ssl/$(bashio::config 'certfile'" "$file"
+sed -i "/server/a KEY_FILE = /ssl/$(bashio::config 'keyfile'" "$file"
+else
+sed -i "/server/a PROTOCOL = http" "$file"
 fi
+
+done
 
 ##############
 # LAUNCH APP #
