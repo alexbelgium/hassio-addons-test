1eed92edb0d6361ce8caf8abb93819a18973698e
Alexandre <alexandre.pary@gmail.com>
2022-11-01 17:41:53 +0100

    test

--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-bashio::log.info "Defining variables"
+#bashio::log.info "Defining variables"
 # Implement images modification
-if bashio::config.has_value 'PUID'; then sed -i "1a export USERMAP_UID=$(bashio::config 'PUID')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'PGID'; then sed -i "1a export USERMAP_GID=$(bashio::config 'PGID')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'TZ'; then sed -i "1a export PAPERLESS_TIME_ZONE=$(bashio::config 'TZ')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'OCRLANG'; then sed -i "1a export PAPERLESS_OCR_LANGUAGES=$(bashio::config 'OCRLANG')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'PAPERLESS_OCR_MODE'; then sed -i "1a export PAPERLESS_OCR_MODE=$(bashio::config 'PAPERLESS_OCR_MODE')" /sbin/docker-entrypoint.sh; fi
+#if bashio::config.has_value 'PUID'; then sed -i "1a export USERMAP_UID=$(bashio::config 'PUID')" /sbin/docker-entrypoint.sh; fi
+#if bashio::config.has_value 'PGID'; then sed -i "1a export USERMAP_GID=$(bashio::config 'PGID')" /sbin/docker-entrypoint.sh; fi
+#if bashio::config.has_value 'TZ'; then sed -i "1a export PAPERLESS_TIME_ZONE=$(bashio::config 'TZ')" /sbin/docker-entrypoint.sh; fi
+#if bashio::config.has_value 'OCRLANG'; then sed -i "1a export PAPERLESS_OCR_LANGUAGES=$(bashio::config 'OCRLANG')" /sbin/docker-entrypoint.sh; fi
+#if bashio::config.has_value 'PAPERLESS_OCR_MODE'; then sed -i "1a export PAPERLESS_OCR_MODE=$(bashio::config 'PAPERLESS_OCR_MODE')" /sbin/docker-entrypoint.sh; fi
 
 # Force folders
-sed -i "1a export PAPERLESS_DATA_DIR=/config/addons_config/paperless_ng" /sbin/docker-entrypoint.sh
-sed -i "1a export PAPERLESS_MEDIA_ROOT=/config/addons_config/paperless_ng/media" /sbin/docker-entrypoint.sh
-sed -i "1a export PAPERLESS_CONSUMPTION_DIR=/config/addons_config/paperless_ng/consume" /sbin/docker-entrypoint.sh
+#sed -i "1a export PAPERLESS_DATA_DIR=/config/addons_config/paperless_ng" /sbin/docker-entrypoint.sh
+#sed -i "1a export PAPERLESS_MEDIA_ROOT=/config/addons_config/paperless_ng/media" /sbin/docker-entrypoint.sh
+#sed -i "1a export PAPERLESS_CONSUMPTION_DIR=/config/addons_config/paperless_ng/consume" /sbin/docker-entrypoint.sh
 
 # Install redis
 bashio::log.info "Installing redis"
@@ -20,4 +20,4 @@ apt-get update && apt-get install -yqq redis-server >/dev/null
 exec redis-server & bashio::log.info "Starting redis"
 
 bashio::log.info "Initial username and password are admin. Please change in the administration panel of the webUI after login."
-/./sbin/docker-entrypoint.sh
+#/./sbin/docker-entrypoint.sh
