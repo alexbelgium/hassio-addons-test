b905ffa0b7cdfa4fcb1edf461f47ece81aa041ad
Alexandre <alexandre.pary@gmail.com>
2022-11-01 11:07:04 +0100

    Correct

--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -5,10 +5,10 @@ bashio::log.info "Initial username and password are admin. Please change in the 
 
 # Implement images modification
 if bashio::config.has_value 'PUID'; then sed -i "1a export USERMAP_UID=$(bashio::config 'PUID')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'PGID'; sed -i "1a export USERMAP_GID=$(bashio::config 'PGID')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'TZ'; sed -i "1a export PAPERLESS_TIME_ZONE=$(bashio::config 'TZ')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'OCRLANG'; sed -i "1a export PAPERLESS_OCR_LANGUAGES=$(bashio::config 'OCRLANG')" /sbin/docker-entrypoint.sh; fi
-if bashio::config.has_value 'PAPERLESS_OCR_MODE'; sed -i "1a export PAPERLESS_OCR_MODE=$(bashio::config 'PAPERLESS_OCR_MODE')" /sbin/docker-entrypoint.sh; fi
+if bashio::config.has_value 'PGID'; then sed -i "1a export USERMAP_GID=$(bashio::config 'PGID')" /sbin/docker-entrypoint.sh; fi
+if bashio::config.has_value 'TZ'; then sed -i "1a export PAPERLESS_TIME_ZONE=$(bashio::config 'TZ')" /sbin/docker-entrypoint.sh; fi
+if bashio::config.has_value 'OCRLANG'; then sed -i "1a export PAPERLESS_OCR_LANGUAGES=$(bashio::config 'OCRLANG')" /sbin/docker-entrypoint.sh; fi
+if bashio::config.has_value 'PAPERLESS_OCR_MODE'; then sed -i "1a export PAPERLESS_OCR_MODE=$(bashio::config 'PAPERLESS_OCR_MODE')" /sbin/docker-entrypoint.sh; fi
 
 # Force folders
 sed -i "1a export PAPERLESS_DATA_DIR=/config/addons_config/paperless_ng" /sbin/docker-entrypoint.sh
