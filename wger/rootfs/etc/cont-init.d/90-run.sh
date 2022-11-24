6f6fcad8947eeeb5a2171febf49b7014f1e550c1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-24 21:37:23 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -14,6 +14,4 @@ export DJANGO_DB_DATABASE="/data/database.sqlite"
 #############
 # Start app #
 #############
-(set -o posix; export -p) > /env.sh
-chmod 777 /env.sh
-/bin/su -s /bin/bash -c '/env.sh; /home/wger/entrypoint.sh' wger
+/bin/su -s /bin/bash -c '/home/wger/entrypoint.sh' wger
