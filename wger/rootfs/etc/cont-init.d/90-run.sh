4acea6f46933e22eb03c8c1c86137809ff1a93e1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-24 21:38:15 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bashio
 
-#####################
-# Align permissions #
-#####################
-chown -R wger:wger /data
-chmod -R 777 /data
-
 ############################
 # Change database location #
 ############################
 export DJANGO_DB_DATABASE="/data/database.sqlite"
 touch /data/database.sqlite
+
+#####################
+# Align permissions #
+#####################
+chown -R wger:wger /data
+chmod -R 777 /data
 
 #############
 # Start app #
