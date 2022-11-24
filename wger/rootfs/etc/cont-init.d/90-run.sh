4f1439875dd9a9b5be888b79562b8d133228f929
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-24 23:01:17 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -3,17 +3,16 @@
 ############################
 # Change database location #
 ############################
-#export DJANGO_DB_DATABASE="/data/database.sqlite"
-#touch /data/database.sqlite
+export DJANGO_DB_DATABASE="/data/database.sqlite"
+touch /data/database.sqlite
 
 #####################
 # Align permissions #
 #####################
-#chown -R wger:wger /data
-#chmod -R 777 /data
+chown -R wger:wger /data
+chmod -R 777 /data
 
 #############
 # Start app #
 #############
-#/bin/su -s /bin/bash -c '/home/wger/entrypoint.sh' wger
 /./home/wger/entrypoint.sh
