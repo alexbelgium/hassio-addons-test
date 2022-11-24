0a000dbc95e5f83b0c3935c561fd7dae8d34d372
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-24 13:26:22 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -21,19 +21,21 @@ sed -i "1a cd /home/wger/src" /home/wger/entrypoint.sh
 echo "Setting static"
 rmdir /home/wger/static && ln -s /home/wger/src/wger/core/static /home/wger || true
 
-su -l wger -c "\
-  cd /home/wger/src && \
-  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
-  export DEBIAN_FRONTEND=noninteractive && \
-  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
-  export LANG=en_US.UTF-8 && \
-  export LANGUAGE=en_US:en && \
-  export LC_ALL=en_US.UTF-8 && \
-  export PATH=/home/wger/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin && \
-  export PYTHONDONTWRITEBYTECODE=1 && \
-  export PYTHONUNBUFFERED=1 && \
-  export DJANGO_DB_DATABASE=/data/database.sqlite && \
-  /bin/bash /home/wger/entrypoint.sh \
-  "
+#su -l wger -c "\
+#  cd /home/wger/src && \
+#  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
+#  export DEBIAN_FRONTEND=noninteractive && \
+#  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
+#  export LANG=en_US.UTF-8 && \
+#  export LANGUAGE=en_US:en && \
+#  export LC_ALL=en_US.UTF-8 && \
+#  export PATH=/home/wger/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin && \
+#  export PYTHONDONTWRITEBYTECODE=1 && \
+#  export PYTHONUNBUFFERED=1 && \
+#  export DJANGO_DB_DATABASE=/data/database.sqlite && \
+#  /bin/bash /home/wger/entrypoint.sh \
+#  "
+  
+/bin/su -s /bin/bash -c 'cd /home/wger/src && /home/wger/entrypoint.sh' wger
 
-/./home/wger/entrypoint.sh
+#/./home/wger/entrypoint.sh
