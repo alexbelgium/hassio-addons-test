d4bca6abd17e93381d8e7b309f695938f2455cdd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-24 13:28:03 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -21,6 +21,9 @@ sed -i "1a cd /home/wger/src" /home/wger/entrypoint.sh
 echo "Setting static"
 rmdir /home/wger/static && ln -s /home/wger/src/wger/core/static /home/wger || true
 
+(set -o posix; export -p) > /env.sh
+chmod 777 /env.sh
+    
 #su -l wger -c "\
 #  cd /home/wger/src && \
 #  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
@@ -36,6 +39,6 @@ rmdir /home/wger/static && ln -s /home/wger/src/wger/core/static /home/wger || t
 #  /bin/bash /home/wger/entrypoint.sh \
 #  "
   
-/bin/su -s /bin/bash -c 'cd /home/wger/src && /home/wger/entrypoint.sh' wger
+/bin/su -s /bin/bash -c '/./env.sh; cd /home/wger/src && /home/wger/entrypoint.sh' wger
 
 #/./home/wger/entrypoint.sh
