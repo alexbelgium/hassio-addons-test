29388ae7c905aff22f475dd661d50ff2ab5693ea
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-24 15:56:41 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -1,44 +1,19 @@
 #!/usr/bin/env bashio
 
-#LOCATION=/data
-#mkdir -p "$LOCATION"
-#echo "Defining database"
-#touch "$LOCATION"/database.sqlite
-#ln -s "$LOCATION"/database.sqlite /home/wger/db
+#####################
+# Align permissions #
+#####################
+chown -R wger:wger /data
+chmod -R 777 /data
 
-#echo "Updating database"
-#python3 manage.py migrate || true
+############################
+# Change database location #
+############################
+export DJANGO_DB_DATABASE="/data/database.sqlite"
 
-#echo "Defining permissions"
-#chown -R wger:wger "$LOCATION"
-#chown -R wger:wger "/home/wger"
-#chmod -R 777 "$LOCATION"
-
-#echo "Launch app"
-#pip install -e . &>/dev/null
-#sed -i "1a cd /home/wger/src" /home/wger/entrypoint.sh
-
-#echo "Setting static"
-#rmdir /home/wger/static && ln -s /home/wger/src/wger/core/static /home/wger || true
-
+#############
+# Start app #
+#############
 (set -o posix; export -p) > /env.sh
 chmod 777 /env.sh
-    
-#su -l wger -c "\
-#  cd /home/wger/src && \
-#  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
-#  export DEBIAN_FRONTEND=noninteractive && \
-#  export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
-#  export LANG=en_US.UTF-8 && \
-#  export LANGUAGE=en_US:en && \
-#  export LC_ALL=en_US.UTF-8 && \
-#  export PATH=/home/wger/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin && \
-#  export PYTHONDONTWRITEBYTECODE=1 && \
-#  export PYTHONUNBUFFERED=1 && \
-#  export DJANGO_DB_DATABASE=/data/database.sqlite && \
-#  /bin/bash /home/wger/entrypoint.sh \
-#  "
-  
 /bin/su -s /bin/bash -c '/./env.sh; cd /home/wger/src && /home/wger/entrypoint.sh' wger
-
-#/./home/wger/entrypoint.sh
