efa1026320e7a6e215a66457f57437bfa92a202d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-25 20:52:49 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -6,9 +6,16 @@
 touch /data/database.sqlite
 sed -i "s|/home/wger/db/database.sqlite|/data/database.sqlite|g" /home/wger/src/settings.py
 
+
 #####################
 # Align permissions #
 #####################
 (set -o posix; export -p) > /data/env.sh
 chown -R 1000:1000 /data
 chmod -R 777 /data
+
+############################
+# Merge static directories #
+############################
+cp -rnf /home/wger/src/wger/core/static/* /data/static
+cp -rnf /home/wger/src/static/* /data/static
