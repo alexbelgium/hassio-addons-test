ffb771a82ce5a48ba04edde3062f1e86a7b66c97
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-25 19:25:58 +0100

    Update 90-run.sh

--- wger/rootfs/etc/cont-init.d/90-run.sh
+++ wger/rootfs/etc/cont-init.d/90-run.sh
@@ -3,7 +3,6 @@
 ############################
 # Change database location #
 ############################
-#export DJANGO_DB_DATABASE="/data/database.sqlite"
 touch /data/database.sqlite
 sed -i "s|/home/wger/db/database.sqlite|/data/database.sqlite|g" /home/wger/src/settings.py
 
@@ -13,12 +12,3 @@ sed -i "s|/home/wger/db/database.sqlite|/data/database.sqlite|g" /home/wger/src/
 (set -o posix; export -p) > /data/env.sh
 chown -R 1000:1000 /data
 chmod -R 777 /data
-
-#sed -i "s|wger:x:0:0|wger:x:1000:1000|g" /etc/passwd
-#usermod -u 1000 wger
-#groupmod -g 1000 wger
-
-#############
-# Start app #
-#############
-sudo -E -H -u wger bash -c ". /data/env.sh; /home/wger/entrypoint.sh"
