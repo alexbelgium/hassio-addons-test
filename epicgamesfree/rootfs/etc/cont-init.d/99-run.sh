9b761f3f14bc5350adf11bf75e7619e7d94f9904
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-12 12:13:17 +0100

    clean

---
+++ epicgamesfree/rootfs/etc/cont-init.d/99-run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bashio
+# shellcheck shell=bash
+
+##############
+# Initialize #
+##############
+
+HOME="/config/addons_config/epicgamesfree"
+if [ ! -f "$HOME"/config.json ]; then
+    # Copy default config.json
+    cp /templates/config.json "$HOME"/config.json
+    chmod 777 "$HOME"/config.json
+    bashio::log.warning "A default config.json file was copied in $HOME. Please customize according to https://github.com/claabs/epicgames-freegames-node#json-configuration before restarting the addon"
+    bashio::exit.nok
+fi
+
+# Make symlink for cookies
+bashio::log.info "The following json files were found in $HOME and will be used in the app. Changing those files will require to restart the addon."
+cd "$HOME" || true
+for i in *.json; do # Whitespace-safe but not recursive.
+    echo "... processing $i"
+    cp "$i" /usr/app/config
+done
+
+# Permissions
+chmod -R 777 "$HOME"
+
+##############
+# Launch App #
+##############
+
+echo " "
+bashio::log.info "Starting the app"
+echo " "
+
+cd /usr/app/config || true
+
+#/./usr/local/bin/docker-entrypoint.sh
--- gitea/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-## 1.17.2 (07-09-2022)
-- Update to version 1.17.2 from go-gitea/gitea
-
-## 1.17.1 (01-09-2022)
-- Update to latest version from go-gitea/gitea
