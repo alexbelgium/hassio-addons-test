2419b9a23018dbd1b2914db7a06eab693a3ac5ba
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-19 20:07:32 +0100

    Update 32-ingress.sh

--- tandoor_recipes/rootfs/etc/cont-init.d/32-ingress.sh
+++ tandoor_recipes/rootfs/etc/cont-init.d/32-ingress.sh
@@ -5,9 +5,9 @@
 # ADAPT SETTINGS #
 ##################
 
-sed -i "/SECURE_CROSS_ORIGIN_OPENER_POLICY/d" /opt/recipes/recipes/settings.py
-sed -i "/CsrfViewMiddleware/d" /opt/recipes/recipes/settings.py
-sed -i "/CROS_ORIGIN_ALLOW_ALL/a SECURE_CROSS_ORIGIN_OPENER_POLICY = None" /opt/recipes/recipes/settings.py
+#sed -i "/SECURE_CROSS_ORIGIN_OPENER_POLICY/d" /opt/recipes/recipes/settings.py
+#sed -i "/CsrfViewMiddleware/d" /opt/recipes/recipes/settings.py
+#sed -i "/CROS_ORIGIN_ALLOW_ALL/a SECURE_CROSS_ORIGIN_OPENER_POLICY = None" /opt/recipes/recipes/settings.py
 
 #################
 # NGINX SETTING #
@@ -22,4 +22,4 @@ sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
 
-sed -i "1a export SCRIPT_NAME=${ingress_entry}" /etc/cont-init.d/99-run.sh
+#sed -i "1a export SCRIPT_NAME=${ingress_entry}" /etc/cont-init.d/99-run.sh
