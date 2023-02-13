7357bc40a25e9e81d66e9c9e6a56cf44646b643f
Alexandre <alexandre.pary@gmail.com>
2023-02-13 11:51:14 +0100

    ingress

--- nextcloud/rootfs/etc/cont-init.d/32-ingress.sh
+++ nextcloud/rootfs/etc/cont-init.d/32-ingress.sh
@@ -13,3 +13,5 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf || true
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf || true
+
+mv /etc/nginx/servers/ingress.conf /data/config/nginx/site-confs/
