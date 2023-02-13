958bfa0b5f1987f83602f392a12fcf07ab8093a6
Alexandre <alexandre.pary@gmail.com>
2023-02-13 10:39:38 +0100

    update

--- nextcloud/rootfs/etc/cont-init.d/32-ingress.sh
+++ nextcloud/rootfs/etc/cont-init.d/32-ingress.sh
@@ -11,5 +11,5 @@ declare ingress_port
 echo "Adapting for ingress"
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf || true
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf || true
