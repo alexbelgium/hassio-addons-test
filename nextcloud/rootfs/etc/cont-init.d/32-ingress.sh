53e764a21f06f96417f1334be3cf6128e517f0a5
Alexandre <alexandre.pary@gmail.com>
2023-02-13 12:26:41 +0100

    update

--- nextcloud/rootfs/etc/cont-init.d/32-ingress.sh
+++ nextcloud/rootfs/etc/cont-init.d/32-ingress.sh
@@ -5,13 +5,18 @@
 # NGINX SETTING #
 #################
 
-declare ingress_interface
-declare ingress_port
-
 echo "Adapting for ingress"
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf || true
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf || true
 
+# Move file
 mv /etc/nginx/servers/ingress.conf /data/config/nginx/site-confs/
+
+# Correct nginx.conf
+if [ -f /data/config/nginx/nginx.conf ]; then
+    sed -i "s|error_log /config/log/nginx/error.log|error_log /proc/1/fd/1 error|g" /data/config/nginx/nginx.conf
+    sed -i "s|pid /run/nginx.pid|pid /var/run/nginx.pid|g" /data/config/nginx/nginx.conf
+    sed -i "1a env HASSIO_TOKEN;" /data/config/nginx/nginx.conf
+fi
