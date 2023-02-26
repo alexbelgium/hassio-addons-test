3b0a2721d446d6b67c6b255fd04659f165e929e4
Alexandre <alexandre.pary@gmail.com>
2023-02-26 12:25:15 +0100

    update

--- binance-trading-bot/rootfs/etc/cont-init.d/30-mongodb.sh
+++ binance-trading-bot/rootfs/etc/cont-init.d/30-mongodb.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bashio
 # shellcheck shell=bash
 
-echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/main' >> /etc/apk/repositories
-echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/community' >> /etc/apk/repositories
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.9/main' >> /etc/apk/repositories
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.9/community' >> /etc/apk/repositories
 
-apk add mongodb mongodb-tools --no-cache
+apk add mongodb mongodb-tools --no-cache >/dev/null
 mkdir -p /data/db/
