9270d0390a10c4e1dd8967e8d5d8dd5a9cdcbf89
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-05-03 15:59:04 +0200

    Update 00-config_location.sh

--- jellyseerr/rootfs/etc/cont-init.d/00-config_location.sh
+++ jellyseerr/rootfs/etc/cont-init.d/00-config_location.sh
@@ -7,8 +7,3 @@ bashio::log.info "Config stored in $CONFIG_LOCATION"
 mkdir -p "$CONFIG_LOCATION"
 chown -R abc:abc "$CONFIG_LOCATION"
 chmod -R 755 "$CONFIG_LOCATION"
-
-# shellcheck disable=SC2013
-for file in $(grep -Esril "/config/addons_config/jellyseerr" /etc/logrotate.d /defaults /etc/cont-init.d /etc/services.d /etc/s6-overlay/s6-rc.d); do
-    sed -i "s=/config/addons_config/jellyseerr=$CONFIG_LOCATION=g" "$file"
-done
