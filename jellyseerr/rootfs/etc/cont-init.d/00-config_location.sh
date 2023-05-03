80fdf39ce3520dea80fb76378a43d284e3637adc
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-05-03 17:16:35 +0200

    Update 00-config_location.sh

--- jellyseerr/rootfs/etc/cont-init.d/00-config_location.sh
+++ jellyseerr/rootfs/etc/cont-init.d/00-config_location.sh
@@ -5,3 +5,5 @@ CONFIG_LOCATION=$(bashio::config 'CONFIG_LOCATION')
 bashio::log.info "Config stored in $CONFIG_LOCATION"
 mkdir -p "$CONFIG_LOCATION"
 cp -rn /app/config/* "$CONFIG_LOCATION"
+rm -r /app/config
+ln -s "$CONFIG_LOCATION" /app/config
