cee21a6cbdb074d1fbadfee7ff88cd92f347d12b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-05-03 17:33:46 +0200

    Update 90-run.sh

--- jellyseerr/rootfs/etc/cont-init.d/90-run.sh
+++ jellyseerr/rootfs/etc/cont-init.d/90-run.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
+# Create files
+CONFIG_LOCATION="/config/addons_config/jellyseer"
+bashio::log.info "Config stored in $CONFIG_LOCATION"
+mkdir -p "$CONFIG_LOCATION"
+cp -rn /app/config/* "$CONFIG_LOCATION"
+cp -rn /app/* /data/
+rm -r /app/config
+ln -s "$CONFIG_LOCATION" /data/config
+
+# Create files
 export JELLYFIN_TYPE=$(bashio::config 'TYPE')
 export TZ=$(bashio::config 'TZ')
 
