fad1a75ffa31b98abcd64595e8799ea626972d5e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-05-03 18:03:49 +0200

    Update 90-run.sh

--- jellyseerr/rootfs/etc/cont-init.d/90-run.sh
+++ jellyseerr/rootfs/etc/cont-init.d/90-run.sh
@@ -5,9 +5,10 @@
 CONFIG_LOCATION="/config/addons_config/jellyseer"
 bashio::log.info "Config stored in $CONFIG_LOCATION"
 mkdir -p "$CONFIG_LOCATION"
-cp -rn /app/config/* "$CONFIG_LOCATION"
-cp -rn /app/* /data/
-rm -r /app/config
+cp -rnT /app/config "$CONFIG_LOCATION"
+cp -rnT /app /data
+rm -r /app
+rm -r /data/config
 ln -s "$CONFIG_LOCATION" /data/config
 
 # Create files
