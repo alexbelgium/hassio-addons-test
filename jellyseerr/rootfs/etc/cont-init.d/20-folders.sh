30615846f752dc4c816f0b160b5dc2331560a59a
Alexandre <alexandre.pary@gmail.com>
2023-05-03 17:27:53 +0200

    update

--- jellyseerr/rootfs/etc/cont-init.d/20-folders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d /config/addons_config/jellyseerr ]; then
-    echo "Creating /config/addons_config/jellyseerr"
-    mkdir -p /config/addons_config/jellyseerr
-fi
-
-cp -rn /app/config/* /config/addons_config/jellyseerr/
