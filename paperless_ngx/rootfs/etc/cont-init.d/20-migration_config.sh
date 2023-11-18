d250f70af4d3ceac1b71eea67322133a6c60da6c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-18 11:47:40 +0100

    update

--- paperless_ngx/rootfs/etc/cont-init.d/20-migration_config.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/20-migration_config.sh
@@ -6,7 +6,5 @@ slug=paperless_ng
 if [ -d /homeassistant/addons_config/$slug ]; then
     echo "Migrating /homeassistant/addons_config/$slug"
     mv /homeassistant/addons_config/$slug/* /config/ || true
-    rm /homeassistant/addons_config/$slug
+    rm -r /homeassistant/addons_config/$slug
 fi
-
-chown -R paperless:paperless /config
