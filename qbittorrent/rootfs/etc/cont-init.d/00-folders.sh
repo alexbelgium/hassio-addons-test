9ee7ec3f888fb1140dee52009d2aeb4555a6d9c7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-12-19 10:02:30 +0100

    Update 00-folders.sh

--- qbittorrent/rootfs/etc/cont-init.d/00-folders.sh
+++ qbittorrent/rootfs/etc/cont-init.d/00-folders.sh
@@ -12,30 +12,28 @@ if [ -f /homeassistant/addons_config/qBittorrent/qBittorrent.conf ] && [ ! -f /h
     bashio::log.warning "----------------------------------------"
     bashio::log.warning "Migrating configuration to the new addon"
     bashio::log.warning "----------------------------------------"
-    cp -rnf /homeassistant/addons_config/qBittorrent/*.json /config/qBittorrent/
-    cp -rnf /homeassistant/addons_config/qBittorrent/*.conf /config/qBittorrent/
-    if [ -d /homeassistant/addons_config/qBittorrent/rss ]; then cp -rnf /homeassistant/addons_config/qBittorrent/rss /config/qBittorrent/; fi
-    if [ -d /config/qBittorrent/data/addons_config ]; then rm -r /config/qBittorrent/data/addons_config; fi
-    if [ -d /config/qBittorrent/data/qBittorrent ]; then rm -r /config/qBittorrent/data/qBittorrent; fi
+    cp -rnf /homeassistant/addons_config/qBittorrent/* /config/qBittorrent/
+    if [ -d /config/qBittorrent/addons_config ]; then rm -r /config/qBittorrent/addons_config; fi
     if [ -d /config/qBittorrent/qBittorrent ]; then rm -r /config/qBittorrent/qBittorrent; fi
-    cp -rnf /homeassistant/addons_config/qBittorrent/* /config/qBittorrent/data/
-    touch /homeassistant/addons_config/qBittorrent/migrated
+    echo "Files moved to /addon_configs/$HOSTNAME/openvpn" > /homeassistant/addons_config/qBittorrent/migrated
     bashio::log.yellow "... moved files from /config/addons_config/qBittorrent to /addon_configs/$HOSTNAME/qBitorrent (must be accessed with my Filebrowser addon)"
     MIGRATED=true
 fi
 
 # Move config
 if [ -f /config/qBittorrent/config/qBittorrent.conf ]; then
-    mv /config/qBittorrent/config/* /config/qBittorrent
+    mv /config/qBittorrent/config/* /config/qBittorrent/
+    mv /config/qBittorrent/data/* /config/qBittorrent/
     rm -r /config/qBittorrent/config
+    rm -r /config/qBittorrent/data
     MIGRATED=true
 fi
 
 # Move openvpn
 if [ -d /homeassistant/openvpn ]; then
     if [ ! -f /homeassistant/openvpn/migrated ] && [ "$(ls -A /homeassistant/openvpn)" ]; then
-        cp -rnf /homeassistant/openvpn/* /config/openvpn/
-        touch /homeassistant/openvpn/migrated
+        cp -rnf /homeassistant/openvpn/* /config/openvpn
+        echo "Files moved to /addon_configs/$HOSTNAME/openvpn" > /homeassistant/openvpn/migrated
     fi
 fi
 
@@ -57,5 +55,6 @@ fi
 # Reboot post migration
 if [[ "$MIGRATED" == "true" ]]; then
     bashio::log.warning "Options were changed, restarting the addon"
+    sleep 5
     bashio::addon.restart
 fi
