1fbd7e17d4d31820f133cc398117df163e963d93
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-10 19:25:47 +0100

    Update 99-run.sh

--- 2fauth/rootfs/etc/cont-init.d/99-run.sh
+++ 2fauth/rootfs/etc/cont-init.d/99-run.sh
@@ -3,10 +3,6 @@
 # shellcheck disable=SC2086
 set -e
 
-echo "Setting permissions"
-chmod -R 777 /config
-chown -R 1000:1000 /config
-
 # Check if APP_KEY is still default, if yes then generate a new one
 if [[ "$(bashio::config "APP_KEY")" == *"SomeRandomStringOf32CharsExactly"* ]]; then
     bashio::log.warning "APP_KEY is still default, a new one will be generated with a backup stored in /config"
@@ -30,6 +26,15 @@ if [[ "$(bashio::config "APP_KEY")" == *"SomeRandomStringOf32CharsExactly"* ]]; 
     bashio::addon.restart
 fi
 
-cp /.env /srv/.env
+#####################
+# Align permissions #
+#####################
+(set -o posix; export -p) > /srv/.env
+echo "Setting permissions"
+chown -R 1000:1000 /config
+chmod -R 777 /config
+
+bashio::log.info "Starting nginx"
+nginx || true & true
 
 bashio::log.info "Starting app"
