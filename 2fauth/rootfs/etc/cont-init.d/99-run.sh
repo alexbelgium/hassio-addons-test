cae819cbd339b11fdd1d403fa9c375ca395f5dce
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-10 16:37:17 +0100

    Update 99-run.sh

--- 2fauth/rootfs/etc/cont-init.d/99-run.sh
+++ 2fauth/rootfs/etc/cont-init.d/99-run.sh
@@ -7,4 +7,13 @@ echo "Setting permissions"
 chmod -R 777 /config
 chown -R 1000:1000 /config
 
+# Check if APP_KEY is still default, if yes then generate a new one
+if [[ "$(bashio::config "APP_KEY")" == "SomeRandomStringOf32CharsExactly" ]]; then
+    bashio::log.warning "APP_KEY is still default, a new one will be generated with a backup stored in /config"
+    APP_KEY="$(php artisan key:generate)"
+    echo "$APP_KEY" > /config/APP_KEY
+    bashio::addon.option "APP_KEY" "$APP_KEY"
+    bashio::addon.restart
+fi
+
 bashio::log.info "Starting app"
