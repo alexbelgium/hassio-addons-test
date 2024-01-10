606fedb21ea5e61752a574eb5b7f877c446933df
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-10 11:58:33 +0100

    Update 99-run.sh

--- 2fauth/rootfs/etc/cont-init.d/99-run.sh
+++ 2fauth/rootfs/etc/cont-init.d/99-run.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
+# shellcheck disable=SC2086
 set -e
+
+bashio::log.info "Starting app"
 
 sudo -u 1000:1000 -s /bin/sh -c "/usr/local/bin/entrypoint.sh"
