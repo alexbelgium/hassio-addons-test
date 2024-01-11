d55c95fb40850b117b345e91a487dffab04d10a6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-11 16:41:16 +0100

    Update 99-run.sh

--- 2fauth/rootfs/etc/cont-init.d/99-run.sh
+++ 2fauth/rootfs/etc/cont-init.d/99-run.sh
@@ -3,6 +3,35 @@
 # shellcheck disable=SC2086
 set -e
 
+#################
+# NGINX SETTING #
+#################
+
+declare ingress_interface
+declare ingress_port
+
+FB_BASEURL=$(bashio::addon.ingress_entry)
+export FB_BASEURL
+
+declare ADDON_PROTOCOL=http
+# Generate Ingress configuration
+if bashio::config.true 'ssl'; then
+    ADDON_PROTOCOL=https
+fi
+
+#port=$(bashio::addon.port 80)
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s|%%protocol%%|${ADDON_PROTOCOL}|g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%subpath%%|${FB_BASEURL}/|g" /etc/nginx/servers/ingress.conf
+mkdir -p /var/log/nginx && touch /var/log/nginx/error.log
+
+###############
+# Set APP_KEY #
+###############
+
 # Check if APP_KEY is still default, if yes then generate a new one
 if [[ "$(bashio::config "APP_KEY")" == *"SomeRandomStringOf32CharsExactl1"* ]]; then
     bashio::log.warning "APP_KEY is still default, a new one will be generated with a backup stored in /config"
@@ -29,6 +58,7 @@ fi
 #####################
 # Align permissions #
 #####################
+
 (set -o posix; export -p) > /srv/.env
 echo "Setting permissions"
 chown -R 1000:1000 /config
