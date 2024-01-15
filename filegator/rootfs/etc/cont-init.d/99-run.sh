8a3e7ee90e7df6397f44347bc8dbd4d6c91d150a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-15 22:19:49 +0100

    Update 99-run.sh

--- filegator/rootfs/etc/cont-init.d/99-run.sh
+++ filegator/rootfs/etc/cont-init.d/99-run.sh
@@ -9,6 +9,11 @@ mkdir -p /config/private /config/repository
 chown -R www-data:www-data /config
 chmod -R 777 /config
 
+rm -r /var/www/filegator/private || true
+ln -s /config/private /var/www/filegator/private || true
+rm -r /var/www/filegator/repository || true
+ln -s /config/private /var/www/filegator/repository || true
+
 bashio::log.info "Starting app. Default login : admin/admin123"
 
 # Start app
