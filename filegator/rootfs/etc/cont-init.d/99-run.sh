f5c251791da1db50f98ed4e95d8b0d2a8308db19
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-16 10:57:31 +0100

    Update 99-run.sh

--- filegator/rootfs/etc/cont-init.d/99-run.sh
+++ filegator/rootfs/etc/cont-init.d/99-run.sh
@@ -4,17 +4,18 @@
 set -e
 
 # Cooy data
-cp -rn /var/www/filegator/* /config/
-mkdir -p /config/private /config/repository
-chown -R www-data:www-data /config
-chmod -R 777 /config
+for folder in repository private; do
+  mkdir -p /config/"$folder"
+  chown -R www-data:www-data /config/"$folder"
+  chmod -R 755 /config/"$folder"
+  cp -rn /var/www/filegator/"$folder"/* /config/"$folder"/
+fi
 
-rm -r /var/www/filegator/private || true
-ln -s /config/private /var/www/filegator/private || true
-rm -r /var/www/filegator/repository || true
-ln -s /config/private /var/www/filegator/repository || true
+# Correct configuration.php
+rm -r /var/www/filegator/configuration.php
+ln -s /config/configuration.php /var/www/filegator/configuration.php
 
 bashio::log.info "Starting app. Default login : admin/admin123"
 
 # Start app
-apache2-foreground docker-php-entrypoint
+apache2-foreground
