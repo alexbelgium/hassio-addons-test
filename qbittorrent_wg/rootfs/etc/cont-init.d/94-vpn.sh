14654342de2b3d7485a0d88d2e9f8271432ee3db
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-17 10:44:00 +0100

    Update 94-vpn.sh

--- qbittorrent_wg/rootfs/etc/cont-init.d/94-vpn.sh
+++ qbittorrent_wg/rootfs/etc/cont-init.d/94-vpn.sh
@@ -15,11 +15,7 @@ if bashio::config.true 'openvpn_enabled' && bashio::config.true 'wireguard_enabl
 fi
 
 # Set variables
-if bashio::config.true 'openvpn_enabled'; then
-    vpn="openvpn"
-    vpn_ending=".ovpn"
-    vpn_interface="tun0"
-else
+if bashio::config.true 'wireguard_enabled'; then
     vpn="wireguard"
     vpn_ending=".conf"
     vpn_interface="wg0"
@@ -33,7 +29,7 @@ chmod 755 /config/wireguard/*
 # CONFIGURE VPN #
 #################
 
-if bashio::config.true 'wireguard_enabled' || bashio::config.true 'openvpn_enabled'; then
+if bashio::config.true 'wireguard_enabled'; then
 
     ############
     # MESSAGES #
@@ -83,164 +79,6 @@ if bashio::config.true 'wireguard_enabled' || bashio::config.true 'openvpn_enabl
     # Send to script
     sed -i "s|/config/${vpn}/config${vpn_ending}|/config/${vpn}/${openvpn_config}|g" /etc/s6-overlay/s6-rc.d/svc-qbittorrent/run
 
-    #####################
-    # OPENVPN SPECIFICS #
-    #####################
-
-    if [[ "$vpn" == "openvpn" ]]; then
-
-        # Check openvpn config file
-        ###########################
-
-        # Double check exists
-        if [ ! -f "/config/${vpn}/${openvpn_config}" ]; then
-            bashio::warning "$file not found"
-            return 1
-        else
-            # Get variable
-            file="/config/${vpn}/${openvpn_config}"
-
-            # Check each lines
-            cp "$file" /tmpfile
-            line_number=0
-            while read -r line; do
-                # Increment the line number
-                ((line_number=line_number+1))
-
-                # Check if lines starting with auth-user-pass have a valid argument
-                ###################################################################
-                if [[ "$line" == "auth-user-pass"* ]]; then
-                    # Extract the second argument
-                    file_name="$(echo "$line" | awk -F' ' '{print $2}')"
-                    # If second argument is null or -
-                    if [ -z "$file_name" ] || [[ "$file_name" == -* ]]; then
-                        # Insert to explain why a comment is made
-                        sed -i "${line_number}i # The following line is commented out as does not contain a valid argument" "$file"
-                        # Increment as new line added
-                        ((line_number=line_number+1))
-                        # Comment out the line
-                        sed -i "${line_number}s/^/# /" "$file"
-                        # Go to next line
-                        continue
-                    fi
-                fi
-
-                # Check if the line contains a txt file
-                #######################################
-                if [[ ! $line =~ ^"#" ]] && [[ ! $line =~ ^";" ]] && [[ "$line" =~ \.txt ]] || [[ "$line" =~ \.crt ]] || [[ "$line" == "auth-user-pass"* ]]; then
-                    # Extract the txt file name from the line
-                    file_name="$(echo "$line" | awk -F' ' '{print $2}')"
-                    # Check if the txt file exists
-                    if [[ "$file_name" != *"/etc/openvpn/credentials"* ]] && [ ! -f "$file_name" ]; then
-                        # Check if the txt file exists in the /config/openvpn/ directory
-                        if [ -f "/config/openvpn/${file_name##*/}" ]; then
-                            # Append /config/openvpn/ in front of the original txt file in the ovpn file
-                            sed -i "${line_number}s|$file_name|/config/openvpn/${file_name##*/}|" "$file"
-                            # Print a success message
-                            bashio::log.warning "Appended /config/openvpn/ to ${file_name##*/} in $file"
-                        else
-                            # Print an error message
-                            bashio::log.warning "$file_name is referenced in your ovpn file but does not exist, and can't be found either in the /config/openvpn/ directory"
-                        fi
-                    fi
-                fi
-            done </tmpfile
-            rm /tmpfile
-
-            # Standardize lf
-            dos2unix "$file"
-
-            # Remove custom up & down
-            sed -i '/^up /s/^/#/' "$file"
-            sed -i '/^down /s/^/#/' "$file"
-
-            # Remove blank lines
-            sed -i '/^[[:blank:]]*$/d' "$file"
-
-            # Ensure config ends with a line feed
-            sed -i "\$q" "$file"
-
-            # Correct paths
-            sed -i "s=/etc/openvpn=/config/openvpn=g" "$file"
-            sed -i "s=/config/openvpn/credentials=/etc/openvpn/credentials=g" "$file"
-        fi
-
-        # Set credentials
-        if bashio::config.has_value "openvpn_username"; then
-            openvpn_username=$(bashio::config 'openvpn_username')
-            echo "${openvpn_username}" >/etc/openvpn/credentials
-        else
-            bashio::exit.nok "Openvpn is enabled, but openvpn_username option is empty! Exiting"
-        fi
-        if bashio::config.has_value "openvpn_password"; then
-            openvpn_password=$(bashio::config 'openvpn_password')
-            echo "${openvpn_password}" >>/etc/openvpn/credentials
-        else
-            bashio::exit.nok "Openvpn is enabled, but openvpn_password option is empty! Exiting"
-        fi
-
-        # Add credentials file
-        if grep -q ^auth-user-pass /config/openvpn/"${openvpn_config}"; then
-            # Credentials specified are they custom ?
-            file_name="$(sed -n "/^auth-user-pass/p" /config/openvpn/"${openvpn_config}" | awk -F' ' '{print $2}')"
-            file_name="${file_name:-null}"
-            if [[ "$file_name" != *"/etc/openvpn/credentials"* ]] && [[ "$file_name" != "null" ]]; then
-                if [ -f "$file_name" ]; then
-                    # If credential specified, exists, and is not the addon default
-                    bashio::log.warning "auth-user-pass specified in the ovpn file, addon username and passwords won't be used !"
-                else
-                    # Credential referenced but doesn't exist
-                    bashio::log.warning "auth-user-pass $file_name is referenced in your ovpn file but does not exist, and can't be found either in the /config/openvpn/ directory. The addon will attempt to use it's own username and password instead."
-                    # Comment previous lines
-                    sed -i '/^auth-user-pass/i # specified auth-user-pass file not found, disabling' /config/openvpn/"${openvpn_config}"
-                    sed -i '/^auth-user-pass/s/^/#/' /config/openvpn/"${openvpn_config}"
-                    # No credentials specified, using addons username and password
-                    echo "# Please do not remove the line below, it allows using the addon username and password" >>/config/openvpn/"${openvpn_config}"
-                    echo "auth-user-pass /etc/openvpn/credentials" >>/etc/openvpn/"${openvpn_config}"
-                fi
-            else
-                # Standardize just to be sure
-                sed -i "/\/etc\/openvpn\/credentials/c auth-user-pass \/etc\/openvpn\/credentials" /config/openvpn/"${openvpn_config}"
-            fi
-        else
-            # No credentials specified, using addons username and password
-            echo "# Please do not remove the line below, it allows using the addon username and password" >>/config/openvpn/"${openvpn_config}"
-            echo "auth-user-pass /etc/openvpn/credentials" >>/config/openvpn/"${openvpn_config}"
-        fi
-
-        # Permissions
-        chmod 755 /etc/openvpn/*
-        chmod 600 /etc/openvpn/credentials
-        chmod 755 /etc/openvpn/*.sh
-        chmod +x /etc/openvpn/*.sh
-
-        # WITH CONTAINER BINDING
-        #########################
-        # If alternative mode enabled, bind container
-        if bashio::config.true 'openvpn_alt_mode'; then
-            echo "Using container binding"
-
-            # Remove interface
-            echo "... deleting previous interface settings"
-            sed -i '/Interface/d' "$QBT_CONFIG_FILE"
-
-            # Modify ovpn config
-            if grep -q route-nopull /config/openvpn/"${openvpn_config}"; then
-                echo "... removing route-nopull from your config.ovpn"
-                sed -i '/route-nopull/d' /config/openvpn/"${openvpn_config}"
-            fi
-
-            # Exit
-            exit 0
-        else
-            # Modify ovpn config
-            if ! grep -q route-nopull /config/openvpn/"${openvpn_config}"; then
-                echo "... adding route-nopull to your config.ovpn"
-                sed -i "1a route-nopull" /config/openvpn/"${openvpn_config}"
-            fi
-        fi
-    fi
-
     #######################
     # WIREGUARD SPECIFICS #
     #######################
@@ -269,7 +107,7 @@ if bashio::config.true 'wireguard_enabled' || bashio::config.true 'openvpn_enabl
         allowed_ips="$(sed -n "/AllowedIPs/p" /config/wireguard/"${openvpn_config}")"
         allowed_ips="${allowed_ips//=*}"
         # Use comma as separator and read into an array
-        IFS=',' read -ra ADDR <<< "$list"
+        IFS=',' read -ra ADDR <<< "$allowed_ips"
         # Initialize an empty array to hold the filtered elements
         filtered=()
         # Loop over the elements
@@ -286,7 +124,7 @@ if bashio::config.true 'wireguard_enabled' || bashio::config.true 'openvpn_enabl
         # Join the filtered elements with commas and store in a variable
         allowed_ips=$(IFS=', '; echo "${filtered[*]}")
         # Store it in the conf file
-        sed -i "|^AllowedIPs|c AllowedIPs=$allowed_ips" /config/openvpn/"${openvpn_config}"
+        sed -i "|^AllowedIPs|c AllowedIPs=$allowed_ips" /config/wireguard/"${openvpn_config}"
 
     fi
 
@@ -294,10 +132,10 @@ if bashio::config.true 'wireguard_enabled' || bashio::config.true 'openvpn_enabl
     # Accept local ip #
     ###################
 
-    ip route add 10.0.0.0/8 via 172.30.32.1
-    ip route add 192.168.0.0/16 via 172.30.32.1
-    ip route add 172.16.0.0/12 via 172.30.32.1
-    ip route add 172.30.0.0/16 via 172.30.32.1
+    ip route add 10.0.0.0/8 via 172.30.32.1 || true
+    ip route add 192.168.0.0/16 via 172.30.32.1 || true
+    ip route add 172.16.0.0/12 via 172.30.32.1 || true
+    ip route add 172.30.0.0/16 via 172.30.32.1 || true
 
     ##################
     # CONFIGURE QBIT #
@@ -331,7 +169,7 @@ if bashio::config.true 'wireguard_enabled' || bashio::config.true 'openvpn_enabl
         exit 1
     fi
 
-else
+elif ! bashio::config.true 'openvpn_enabled'; then
 
     ##################
     # REMOVE OPENVPN #
