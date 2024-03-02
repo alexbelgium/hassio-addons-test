441ebc43b718f6883cf6354fda6a49b9f0cff79f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-03-02 15:04:22 +0100

    Update 20-folders.sh

--- codex/rootfs/etc/cont-init.d/20-folders.sh
+++ codex/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
-
-if [ ! -d /config/addons_config/filebrowser ]; then
-    echo "Creating /config/addons_config/filebrowser"
-    mkdir -p /config/addons_config/filebrowser
-    chmod 777 /config/addons_config/filebrowser
-fi
