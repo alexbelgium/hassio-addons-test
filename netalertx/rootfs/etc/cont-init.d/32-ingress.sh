1d0aa2ba669c128c7cb4517d4732786a9e54ada8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-14 12:06:16 +0100

    Update 32-ingress.sh

--- netalertx/rootfs/etc/cont-init.d/32-ingress.sh
+++ netalertx/rootfs/etc/cont-init.d/32-ingress.sh
@@ -13,6 +13,6 @@ echo "Adapting for ingress"
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 ingress_entry=$(bashio::addon.ingress_entry)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/http.d/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/http.d/ingress.conf
+sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/http.d/ingress.conf
+sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/http.d/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/http.d/ingress.conf
