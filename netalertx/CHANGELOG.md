deeca4a0d88a9c1b705beb08c7dfdd6bc2910b6b
Alexandre <alexandre.pary@gmail.com>
2025-01-14 11:35:20 +0100

    push

---
+++ netalertx/CHANGELOG.md
@@ -0,0 +1,46 @@
+## 25.1.11 (12-01-2025)
+- Moving 99-run.sh to cont-init.d
+
+## 25.1.9 (12-01-2025)
+
+- Minor bugs fixed
+
+## 25.1.8.9 (11-01-2025)
+
+- Docs, symlink test
+
+## 25.1.8 (11-01-2025)
+
+- Update to latest version from jokob-sk/NetAlertX (changelog : https://github.com/jokob-sk/NetAlertX/releases)
+
+## 0.0.8 (10-01-2025)
+
+- Minor bugs fixed
+
+## 0.0.7
+
+- re-add oneshot type
+
+## 0.0.6
+
+- test symlinks
+
+## 0.0.5
+
+- attempt at starting start.sh, back to prod image
+
+## 0.0.4
+
+- removal of unnecessary files WARNING: dev image
+
+## 0.0.3
+
+- removal of app_conf_override
+
+## 0.0.2
+
+- armv6 removal, host network
+
+## 0.0.1
+
+- Initial commit
---
+++ netalertx/rootfs/etc/cont-init.d/99-run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bashio
+# shellcheck shell=bash
+set -e
+
+bashio::log.warning "App starting."
+
+
+
+# In the addon script, make symlinks on the fly
+echo "Creating symlinks"
+for folder in config db; do
+    echo "Creating for $folder"
+    # Create symlinks
+    mkdir -p /config/"$folder"
+    if [ -d /app/"$folder" ] && [ "$(ls -A /app/"$folder")" ]; then
+        cp -rn /app/"$folder"/* /config/"$folder"/
+    fi
+    rm -r /app/"$folder"
+    ln -sf /config/"$folder" /app/"$folder"
+done
+
+sudo chown -R nginx:www-data /config/db/
+sudo chown -R nginx:www-data /config/config/
+if [ -f /config/db/app.db ]; then
+    chmod a+rwx /config/db/app.db
+fi
+
+##############
+# LAUNCH APP #
+##############
+
+#chmod +x /app/dockerfiles/start.sh
+#/app/dockerfiles/start.sh
