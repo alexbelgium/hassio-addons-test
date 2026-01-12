6c61cff9cbb1c51149bb6e04febaa29637806b00
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-01-12 11:48:08 +0100

    Handle missing Karakeep migration script

--- karakeep_wg/rootfs/etc/cont-init.d/30-db-migrate.sh
+++ karakeep_wg/rootfs/etc/cont-init.d/30-db-migrate.sh
@@ -7,10 +7,18 @@ source /usr/lib/bashio/bashio.sh
 PUID=$(bashio::config 'PUID')
 PGID=$(bashio::config 'PGID')
 
-if [[ ! -f /db_migrations/index.js ]]; then
-  bashio::log.error "Karakeep migration script not found"
-  bashio::exit.nok
+migration_script=""
+
+if [[ -f /db_migrations/index.js ]]; then
+  migration_script="/db_migrations/index.js"
+elif [[ -f /app/db_migrations/index.js ]]; then
+  migration_script="/app/db_migrations/index.js"
+fi
+
+if [[ -z "${migration_script}" ]]; then
+  bashio::log.warning "Karakeep migration script not found; skipping migrations"
+  exit 0
 fi
 
 bashio::log.info "Running Karakeep database migrations"
-exec s6-setuidgid "${PUID}:${PGID}" node /db_migrations/index.js
+exec s6-setuidgid "${PUID}:${PGID}" node "${migration_script}"
