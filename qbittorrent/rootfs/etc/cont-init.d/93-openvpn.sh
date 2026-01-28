4edc3de5cc7362ceaae2336af26a005fcb0ea27d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-01-28 13:52:33 +0100

    static https://github.com/alexbelgium/hassio-addons/pull/2399

---
+++ qbittorrent/rootfs/etc/cont-init.d/93-openvpn.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+OPENVPN_STATE_DIR="/var/run/openvpn"
+QBT_CONFIG_FILE="/config/qBittorrent/qBittorrent.conf"
+declare openvpn_config=""
+declare openvpn_runtime_config=""
+declare interface_name=""
+declare openvpn_username
+declare openvpn_password
+
+if bashio::file_exists "${OPENVPN_STATE_DIR}"; then
+    bashio::log.warning "Previous OpenVPN state directory found, cleaning up."
+    rm -Rf "${OPENVPN_STATE_DIR}"
+fi
+
+if ! bashio::config.true 'openvpn_enabled'; then
+    bashio::exit.ok 'OpenVPN is disabled.'
+elif bashio::config.true 'wireguard_enabled'; then
+    bashio::exit.nok 'OpenVPN and WireGuard cannot be enabled simultaneously. Disable one of them.'
+fi
+
+mkdir -p "${OPENVPN_STATE_DIR}"
+
+bashio::log.info "----------------------------"
+bashio::log.info "Openvpn enabled, configuring"
+bashio::log.info "----------------------------"
+
+# Set credentials
+if bashio::config.has_value "openvpn_username"; then
+    openvpn_username=$(bashio::config 'openvpn_username')
+else
+    bashio::exit.nok "Openvpn is enabled, but openvpn_username option is empty! Exiting"
+fi
+if bashio::config.has_value "openvpn_password"; then
+    openvpn_password=$(bashio::config 'openvpn_password')
+else
+    bashio::exit.nok "Openvpn is enabled, but openvpn_password option is empty! Exiting"
+fi
+
+echo -e "${openvpn_username}\n${openvpn_password}" > "${OPENVPN_STATE_DIR}/credentials.conf"
+chmod 600 "${OPENVPN_STATE_DIR}/credentials.conf"
+
+if bashio::config.has_value 'openvpn_config'; then
+    openvpn_config="$(bashio::config 'openvpn_config')"
+    openvpn_config="${openvpn_config##*/}"
+    if [[ -z "${openvpn_config}" ]]; then
+        bashio::log.info 'openvpn_config option left empty. Attempting automatic selection.'
+         mapfile -t configs < <(find /config/openvpn -maxdepth 1 -type f -name '*.conf' -o -name '*.ovpn' -print)
+        if [ "${#configs[@]}" -eq 0 ]; then
+            bashio::exit.nok 'OpenVPN is enabled but no .conf or .ovpn file was found in /config/openvpn.'
+        elif [ "${#configs[@]}" -eq 1 ]; then
+            openvpn_config="${configs[0]}"
+            bashio::log.info "OpenVPN configuration not specified. Using ${openvpn_config##*/}."
+        elif bashio::fs.file_exists '/config/openvpn/config.conf'; then
+            openvpn_config='/config/openvpn/config.conf'
+            bashio::log.info 'Using default OpenVPN configuration config.conf.'
+        else
+            bashio::exit.nok "Multiple OpenVPN configuration files detected. Please set the 'openvpn_config' option."
+        fi
+    elif bashio::fs.file_exists "/config/openvpn/${openvpn_config}"; then
+        openvpn_config="/config/openvpn/${openvpn_config}"
+    else
+        bashio::exit.nok "OpenVPN configuration '/config/openvpn/${openvpn_config}' not found."
+    fi
+fi
+
+interface_name="$(sed -n "/^dev tun/p" "${openvpn_config}" | awk -F' ' '{print $2}')"
+if [[ -z "${interface_name}" ]]; then
+    bashio::exit.nok "OpenVPN configuration '/config/openvpn/${openvpn_config}' miss device directive."
+elif [[ ${interface_name} = "tun" ]]; then
+    interface_name='tun0'
+elif [[ ${interface_name} = "tap" ]]; then
+    interface_name='tap0'
+fi
+
+openvpn_runtime_config="${OPENVPN_STATE_DIR}/${interface_name}.conf"
+
+cp "${openvpn_config}" "${openvpn_runtime_config}"
+chmod 600 "${openvpn_runtime_config}"
+
+dos2unix "${openvpn_runtime_config}" >/dev/null 2>&1 || true
+sed -i '/^[[:space:]]*[;#]/d' "${openvpn_runtime_config}"
+sed -i 's/#.*//' "${openvpn_runtime_config}"
+sed -i '/^[[:space:]]*$/d' "${openvpn_runtime_config}"
+sed -i '/^[[:blank:]]*$/d' "${openvpn_runtime_config}"
+sed -i '/^up/d' "${openvpn_runtime_config}"
+sed -i '/^down/d' "${openvpn_runtime_config}"
+sed -i '/^route/d' "${openvpn_runtime_config}"
+sed -i '/^auth-user-pass /d' "${openvpn_runtime_config}"
+sed -i '/^cd /d' "${openvpn_runtime_config}"
+sed -i '/^chroot /d' "${openvpn_runtime_config}"
+sed -i '\$q' "${openvpn_runtime_config}"
+
+bashio::log.info 'Prepared OpenVPN runtime configuration for initial connection attempt.'
+
+echo "${openvpn_runtime_config}" > "${OPENVPN_STATE_DIR}/config"
+echo "${interface_name}" > "${OPENVPN_STATE_DIR}/interface"
+
+bashio::log.info "Using interface binding in the qBittorrent app"
+
+if bashio::fs.file_exists "${QBT_CONFIG_FILE}"; then
+    sed -i '/Interface/d' "${QBT_CONFIG_FILE}"
+    sed -i "/\\[Preferences\\]/ i\\Connection\\\\Interface=${interface_name}" "${QBT_CONFIG_FILE}"
+    sed -i "/\\[Preferences\\]/ i\\Connection\\\\InterfaceName=${interface_name}" "${QBT_CONFIG_FILE}"
+    sed -i "/\\[BitTorrent\\]/a \\Session\\\\Interface=${interface_name}" "${QBT_CONFIG_FILE}"
+    sed -i "/\\[BitTorrent\\]/a \\Session\\\\InterfaceName=${interface_name}" "${QBT_CONFIG_FILE}"
+else
+    bashio::log.warning "qBittorrent config file not found. Bind the client manually to interface ${interface_name}."
+fi
+
+bashio::log.info "OpenVPN prepared with interface ${interface_name} using configuration ${openvpn_config##*/}."
---
+++ qbittorrent/rootfs/etc/cont-init.d/94-wireguard.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+WIREGUARD_STATE_DIR="/var/run/wireguard"
+QBT_CONFIG_FILE="/config/qBittorrent/qBittorrent.conf"
+declare wireguard_config=""
+declare wireguard_runtime_config=""
+declare interface_name=""
+
+if bashio::file_exists "${WIREGUARD_STATE_DIR}"; then
+    bashio::log.warning "Previous WireGuard state directory found, cleaning up."
+    rm -Rf "${WIREGUARD_STATE_DIR}"
+fi
+
+if ! bashio::config.true 'wireguard_enabled'; then
+    bashio::exit.ok 'WireGuard is disabled.'
+elif bashio::config.true 'openvpn_enabled'; then
+    bashio::exit.nok 'OpenVPN and WireGuard cannot be enabled simultaneously. Disable one of them.'
+fi
+
+mkdir -p "${WIREGUARD_STATE_DIR}"
+
+bashio::log.info "------------------------------"
+bashio::log.info "Wireguard enabled, configuring"
+bashio::log.info "------------------------------"
+
+if bashio::config.has_value 'wireguard_config'; then
+    wireguard_config="$(bashio::config 'wireguard_config')"
+    wireguard_config="${wireguard_config##*/}"
+    if [[ -z "${wireguard_config}" ]]; then
+        bashio::log.info 'wireguard_config option left empty. Attempting automatic selection.'
+         mapfile -t configs < <(find /config/wireguard -maxdepth 1 -type f -name '*.conf' -print)
+        if [ "${#configs[@]}" -eq 0 ]; then
+            bashio::exit.nok 'WireGuard is enabled but no .conf file was found in /config/wireguard.'
+        elif [ "${#configs[@]}" -eq 1 ]; then
+            wireguard_config="${configs[0]}"
+            bashio::log.info "WireGuard configuration not specified. Using ${wireguard_config##*/}."
+        elif bashio::fs.file_exists '/config/wireguard/config.conf'; then
+            wireguard_config='/config/wireguard/config.conf'
+            bashio::log.info 'Using default WireGuard configuration config.conf.'
+        else
+            bashio::exit.nok "Multiple WireGuard configuration files detected. Please set the 'wireguard_config' option."
+        fi
+    elif bashio::fs.file_exists "/config/wireguard/${wireguard_config}"; then
+        wireguard_config="/config/wireguard/${wireguard_config}"
+    else
+        bashio::exit.nok "WireGuard configuration '/config/wireguard/${wireguard_config}' not found."
+    fi
+fi
+
+interface_name="$(basename "${wireguard_config}" .conf)"
+if [[ -z "${interface_name}" ]]; then
+    interface_name='wg0'
+fi
+
+wireguard_runtime_config="${WIREGUARD_STATE_DIR}/${interface_name}.conf"
+
+cp "${wireguard_config}" "${wireguard_runtime_config}"
+chmod 600 "${wireguard_runtime_config}"
+
+dos2unix "${wireguard_runtime_config}" >/dev/null 2>&1 || true
+sed -i '/^[[:space:]]*[;#]/d' "${wireguard_runtime_config}"
+sed -i 's/#.*//' "${wireguard_runtime_config}"
+sed -i '/^[[:space:]]*$/d' "${wireguard_runtime_config}"
+sed -i '/^[[:blank:]]*$/d' "${wireguard_runtime_config}"
+sed -i '/DNS/d' "${wireguard_runtime_config}"
+sed -i '/PostUp/d' "${wireguard_runtime_config}"
+sed -i '/PostDown/d' "${wireguard_runtime_config}"
+sed -i '/SaveConfig/d' "${wireguard_runtime_config}"
+sed -i "\$q" "${wireguard_runtime_config}"
+
+bashio::log.info 'Prepared WireGuard runtime configuration for initial connection attempt.'
+
+echo "${wireguard_runtime_config}" > "${WIREGUARD_STATE_DIR}/config"
+echo "${interface_name}" > "${WIREGUARD_STATE_DIR}/interface"
+
+bashio::log.info "Using interface binding in the qBittorrent app"
+
+if bashio::fs.file_exists "${QBT_CONFIG_FILE}"; then
+    sed -i '/Interface/d' "${QBT_CONFIG_FILE}"
+    sed -i "/\\[Preferences\\]/ i\\Connection\\\\Interface=${interface_name}" "${QBT_CONFIG_FILE}"
+    sed -i "/\\[Preferences\\]/ i\\Connection\\\\InterfaceName=${interface_name}" "${QBT_CONFIG_FILE}"
+    sed -i "/\\[BitTorrent\\]/a \\Session\\\\Interface=${interface_name}" "${QBT_CONFIG_FILE}"
+    sed -i "/\\[BitTorrent\\]/a \\Session\\\\InterfaceName=${interface_name}" "${QBT_CONFIG_FILE}"
+else
+    bashio::log.warning "qBittorrent config file not found. Bind the client manually to interface ${interface_name}."
+fi
+
+bashio::log.info "WireGuard prepared with interface ${interface_name} using configuration ${wireguard_config##*/}."
